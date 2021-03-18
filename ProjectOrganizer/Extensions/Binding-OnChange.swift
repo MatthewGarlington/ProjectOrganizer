64f3cce1bc78f15f02245d0c724c5f7dcbc43d6d
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 17:59:28 -0400

    feat: The added extension gives the ability for the property the modifer is added to be saved upon changing of it's content

--- ProjectOrganizer/Extensions/Binding-OnChange.swift
+++ ProjectOrganizer/Extensions/Binding-OnChange.swift
@@ -5,4 +5,16 @@
 //  Created by Matthew Garlington on 3/18/21.
 //
 
-import Foundation
+import SwiftUI
+
+extension Binding {
+    func onChange(_ handler: @escaping () -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: {  newValue in
+                self.wrappedValue = newValue
+                handler()
+            }
+        )
+    }
+}
