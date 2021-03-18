515610a4905f246a920bb23244f336d31510251c
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 16:47:23 -0400

    feat: The added view displays on the homescreen when the user adds items to projects to be completed
    This view uses all items that are in an open project to display each item with the ability to edit the item upon a press. The Progress ring next to each item title shows the prcentage complete marked with an icon. A custom blur view modifer was added to the background color of each cell the item is in. When no projects are selected an empty view will present until items are added.The progress bar will display in the color that matches the color selected when creating a new project to give the user more context

--- ProjectOrganizer/Activities/Home/ItemListView.swift
+++ ProjectOrganizer/Activities/Home/ItemListView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct ItemListView: View {
+    let title: LocalizedStringKey
+    let items: FetchedResults<Item>.SubSequence
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if items.isEmpty {
+            EmptyView()
+        } else {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.secondary)
+                .padding(.top)
+            ForEach(items) { item in
+                NavigationLink(destination: EditItemView(item: item)) {
+                    HStack(spacing: 20) {
+                        Circle()
+                            .stroke(Color(item.project?.projectColor ?? "Light Blue"), lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                        VStack(alignment: .leading) {
+                            Text(item.itemTitle)
+                                .font(.title2)
+                                .foregroundColor(.primary)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            if item.itemDetail.isEmpty == false {
+                                Text(item.itemDetail)
+                                    .foregroundColor(.secondary )
+                            }
+                        }
+                    }
+                    .padding()
+                    .background(BlurView(style: .systemUltraThinMaterialLight))
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5)
+                }
+            }
+        }
     }
 }
 
-struct ItemListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ItemListView()
-    }
-}
+//struct ItemListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ItemListView()
+//    }
+//}
