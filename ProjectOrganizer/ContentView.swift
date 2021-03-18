302b8da2a28c5ad3de2772776d6ed80eeaca3a2a
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 17:54:01 -0400

    feat: This added tab bar gives the user the choice to choose between Homeview, Open Projects, Closed projects, or Award view

--- ProjectOrganizer/ContentView.swift
+++ ProjectOrganizer/ContentView.swift
@@ -7,15 +7,49 @@
 
 import SwiftUI
 
+
+/// Adds  TabView that allows the user to select between Home, Open Projects, Closed Projects,
+/// and awards that the user has earned
 struct ContentView: View {
+    @SceneStorage("selectedView") var selectedView: String?
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selectedView) {
+            HomeView()
+                .tag(HomeView.tag)
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            ProjectsView(showClosedProjects: false)
+                .tag(ProjectsView.openTag)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Open")
+                }
+            ProjectsView(showClosedProjects: true)
+                .tag(ProjectsView.closedTag)
+                .tabItem {
+                    Image(systemName: "checkmark")
+                    Text("Closed")
+                }
+            AwardsView()
+                .tag(AwardsView.tag)
+                .tabItem {
+                    Image(systemName: "rosette")
+                    Text("Awards")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+    
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
     }
 }
