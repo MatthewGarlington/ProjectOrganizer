9da66b9e94fc95ae1fb23e59ed7686d0d37f3c96
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 17:01:37 -0400

    feat: The added view places Open projects along the top in a Horiontal Scroll View
    The view that displays at the top of the Homescreen gives the title of the projec with a progress view that gives the percentage that the project is complete. The accent color of the given card is given through the editing of each project

--- ProjectOrganizer/Activities/Home/ProjectSummaryView.swift
+++ ProjectOrganizer/Activities/Home/ProjectSummaryView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct ProjectSummaryView: View {
+    @ObservedObject var project: Project
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("\(project.projectItems.count) items")
+                .font(.caption)
+                .foregroundColor(.secondary)
+            Text(project.projectTitle)
+                .font(.title)
+            ProgressView(value: project.completionAmount)
+                .accentColor(Color(project.projectColor))
+        }
+        .padding()
+        .background(BlurView(style: .systemMaterialLight))
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.2), radius: 5)
+        .padding(5)
     }
 }
 
 struct ProjectSummaryView_Previews: PreviewProvider {
     static var previews: some View {
-        ProjectSummaryView()
+        ProjectSummaryView(project: Project.example)
     }
 }
