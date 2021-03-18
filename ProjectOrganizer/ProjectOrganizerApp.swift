c9a3fa93c19841637a023cf4c7f86bd4330eac9e
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 15:30:35 -0400

    init commit

---
+++ ProjectOrganizer/ProjectOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOrganizerApp.swift
+//  ProjectOrganizer
+//
+//  Created by Matthew Garlington on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
