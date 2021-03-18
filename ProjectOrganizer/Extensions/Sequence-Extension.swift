02fc882f71aa9a6524bb78f68c15ff4a6984030e
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 19:15:07 -0400

    feat: Added Sequence extension to call order of sorted function by increasing order

--- ProjectOrganizer/Extensions/Sequence-Extension.swift
+++ ProjectOrganizer/Extensions/Sequence-Extension.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+extension Sequence {
+    func sorted<Value>(by keyPath: KeyPath<Element,
+                                           Value>,
+                       using areInIncreasingOrder: (Value, Value) throws -> Bool)
+    rethrows -> [Element] {
+        try self.sorted {
+            try areInIncreasingOrder($0[keyPath: keyPath], $1[keyPath: keyPath])
+        }
+    }
+    
+    func sorted<Value: Comparable>(by keyPath: KeyPath<Element, Value>) -> [Element] {
+        self.sorted(by: keyPath, using: <)
+    }
+}
