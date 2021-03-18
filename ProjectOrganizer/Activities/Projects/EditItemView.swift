ddcc81c59a75fdf3ca1463f5783048fd08d212ff
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 17:06:48 -0400

    faet: This added view gives the user the option to Edit the current Item that is selected
    This view presents through a Navigation view and gives the ability to set the Item title, description, priority (which will change where the item displays in the itemlist) and if the item is complete. The edited item then syncs to Core Data upon closing of the app or when the user exits the navigation view. The priority is written through the use of a custom onchange modifer that stores the tag of each of the priority being shown in order to be sorted.

--- ProjectOrganizer/Activities/Projects/EditItemView.swift
+++ ProjectOrganizer/Activities/Projects/EditItemView.swift
@@ -5,16 +5,59 @@
 //  Created by Matthew Garlington on 3/18/21.
 //
 
+
 import SwiftUI
 
 struct EditItemView: View {
+    let item: Item
+    @EnvironmentObject var dataController: DataController
+    @State private var title: String
+    @State private var detail: String
+    @State private var priority: Int
+    @State private var completed: Bool
+    
+    init(item: Item) {
+        self.item = item
+        _title = State(wrappedValue: item.itemTitle)
+        _detail = State(wrappedValue: item.itemDetail)
+        _priority = State(wrappedValue: Int(item.priority))
+        _completed = State(wrappedValue: item.completed)
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Basic Settings")) {
+                // The onChange modifier adds a call to the create extension of Binding-onChange which stores the data
+                TextField("Item name", text: $title.onChange(update))
+                TextField("Description", text: $detail.onChange(update))
+            }
+            Section(header: Text("Priority")) {
+                Picker("Priority", selection: $priority.onChange(update)) {
+                    Text("Low").tag(1)
+                    Text("Medium").tag(2)
+                    Text("High").tag(3)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            Section {
+                Toggle("Mark Completed", isOn: $completed.onChange(update))
+            }
+        }
+        .navigationTitle("Edit Item")
+        // Call save to Core Data when view goes away
+        .onDisappear(perform: dataController.save)
+    }
+    func update() {
+        // Notify any view that has this item that it is about to change
+        item.project?.objectWillChange.send()
+        item.title = title
+        item.detail = detail
+        item.priority = Int16(priority)
+        item.completed = completed
     }
 }
 
 struct EditItemView_Previews: PreviewProvider {
     static var previews: some View {
-        EditItemView()
+        EditItemView(item: Item.example)
     }
 }
