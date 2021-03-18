0ff223ba1f36f9047e77cbb40f5ecbbad6b1b5be
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 17:53:05 -0400

    feat: This view was added in order to give the user context that projects must be added to show anything in the open or closed projects tab

--- ProjectOrganizer/Activities/Projects/SelectSomethingView.swift
+++ ProjectOrganizer/Activities/Projects/SelectSomethingView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct SelectSomethingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Please select something from the menu to begin")
+            .italic()
+            .foregroundColor(.secondary)
     }
 }
 
