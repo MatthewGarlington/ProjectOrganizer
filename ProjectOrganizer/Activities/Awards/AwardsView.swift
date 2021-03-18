813d18de02e37846e6b9599c25e2be9e10e367f3
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 16:43:50 -0400

    feat: The added view presents the achieved awards for the user
    This view shows the awards that will be a gray color when the awards are not achieved with the ability to tap each icon and reveal what the user must do to get the award. The award wil turn to the color provided by the Awards.json file when the user meets the criterion for that award. This view was made by adding a foreach loop with that cycles through the different awards and presents them in a lazy grid. An alert is displayed on tap of the button to show the criteria needed to achieve the specific award that is pressed

--- ProjectOrganizer/Activities/Awards/AwardsView.swift
+++ ProjectOrganizer/Activities/Awards/AwardsView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct AwardsView: View {
+    static let tag: String? = "Awards"
+    
+    @EnvironmentObject var dataController: DataController
+    @State private var selectedAward = Award.example
+    @State private var showingAwardDetail = false
+    var colums: [GridItem] {
+        [GridItem(.adaptive(minimum: 100, maximum: 100))]
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: colums) {
+                    ForEach(Award.allAwards) { award in
+                        Button {
+                            selectedAward = award
+                            showingAwardDetail = true
+                        } label: {
+                            Image(systemName: award.image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(color(for: award))
+                              
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Awards")
+        }
+        .alert(isPresented: $showingAwardDetail, content: getAwardAlert)
+    }
+    
+    func color(for award: Award) -> Color {
+        dataController.hasEarned(award: award) ? Color(award.color) : Color.secondary.opacity(0.5)
+        
+    }
+    
+    func getAwardAlert() -> Alert {
+        
+        if dataController.hasEarned(award: selectedAward) {
+            return Alert(title: Text("Unlocked \(selectedAward.name)"),
+                         message: Text("\(selectedAward.description)"),
+                         dismissButton: .default(Text("Ok")))
+        } else {
+            return Alert(title: Text("Locked"), message: Text(selectedAward.description),
+                         dismissButton: .default(Text("Ok")))
+        }
     }
 }
 
@@ -18,3 +65,4 @@ struct AwardsView_Previews: PreviewProvider {
         AwardsView()
     }
 }
+
