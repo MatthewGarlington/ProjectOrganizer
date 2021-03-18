2d7b585e53fd0c340fed2490687ebc42e7b5070c
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 18:00:04 -0400

    style: The added custom extension allows a glass appreance background based off of UIKit blur effect to be used throughout the application

--- ProjectOrganizer/Extensions/Blur-Extension.swift
+++ ProjectOrganizer/Extensions/Blur-Extension.swift
@@ -5,4 +5,36 @@
 //  Created by Matthew Garlington on 3/18/21.
 //
 
-import Foundation
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    
+    typealias UIViewType = UIView
+    var style: UIBlurEffect.Style
+ 
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: CGRect.zero)
+        view.backgroundColor = .clear
+        
+        // adding the style variable makes the blur style customizable
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+       
+        view.insertSubview(blurView, at: 0)
+        
+        NSLayoutConstraint.activate([
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+}
