5a0a214141cbd89711c87bb9aaafc8c40684e78a
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 17:13:24 -0400

    feat: The added view displays all items within the header of their project
    This gives the user the ability to select and edit all the items in the given project that are organized by each project. The user can select whether they want the list to prsent in order of creation date, completion, or optimized which takes into account several factors that make the most relevent items show at the top of the list. The items have an icon next to them to show their completion through a check marked circle when the item is complete in the projects accent color, an exclimation point if the project have a high priority, and no icon when the item is neither complete nor a high priority.

--- ProjectOrganizer/Activities/Projects/ItemRowView.swift
+++ ProjectOrganizer/Activities/Projects/ItemRowView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct ItemRowView: View {
+    @ObservedObject var project: Project
+    @ObservedObject var item: Item
+    
+    var icon: some View {
+        
+        if item.completed {
+            return Image(systemName: "checkmark.circle")
+                .foregroundColor(Color(project.projectColor))
+        } else if item.priority == 3 {
+            return Image(systemName: "exclamationmark.triangle")
+                .foregroundColor(Color(project.projectColor))
+        } else {
+            return Image(systemName: "checkmark.circle")
+                .foregroundColor(.clear)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: EditItemView(item: item)) {
+            Label {
+                Text(item.itemTitle)
+            } icon: {
+                icon
+            }
+        }
     }
 }
 
 struct ItemRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ItemRowView()
+        ItemRowView(project: Project.example, item: Item.example)
     }
 }
