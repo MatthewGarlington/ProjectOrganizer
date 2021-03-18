25e6a2f4dc596b8eafa6f344ff5f5fa2c587038e
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 17:52:14 -0400

    feat: Added this view to display ProjectHeadersView, Item Lists, navigation to editing projects and items, and addition of new projects
    This view is seperated into filtering open projects and closed projects. A new project is able to be added with the plus button that is in the trailing position of the navigation view. In the leading position of the navigation bar there is crossed arrows that allow the user to pick the filter for the list items for optamized, creation date, or completion amount. The add item button at the bottom of the item list allows the user to add a new item that can be edited upon press of the given item to bring up the ItemEditView. The user can swipe to delete an item in the item list. The views to filter open and closed projects are done so using tags that are called in the ContentView. All edits made are saved to the dataController which in turn saves to Core Data so the projects edit and item added will be saved

--- ProjectOrganizer/Activities/Projects/ProjectsView.swift
+++ ProjectOrganizer/Activities/Projects/ProjectsView.swift
@@ -8,13 +8,142 @@
 import SwiftUI
 
 struct ProjectsView: View {
+    static let openTag: String? = "Open"
+    static let closedTag: String? = "Closed"
+    
+    @EnvironmentObject var dataController: DataController
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @State private var showingSortOrder = false
+    @State private var sortOrder = Item.SortOrder.optimized
+    
+    let showClosedProjects: Bool
+    let projects: FetchRequest<Project>
+    
+    init(showClosedProjects: Bool) {
+        self.showClosedProjects = showClosedProjects
+        projects = FetchRequest<Project>(entity: Project.entity(), sortDescriptors: [
+            NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)
+        ], predicate: NSPredicate(format: "closed = %d", showClosedProjects))
+        
+    }
+    
+    
+    var projectsList: some View {
+        
+    
+      
+        List {
+            ForEach(projects.wrappedValue) { project in
+                Section(header: ProjectHeaderView(project: project)) {
+                    ForEach(project.projectItems(using: sortOrder)) { item in
+                        ItemRowView(project: project, item: item)
+                        
+                    }
+                    // delete the items without messing up the sorted order
+                    .onDelete { offsets in
+                        delete(offsets, from: project)
+                    }
+                    if showClosedProjects == false {
+                        Button {
+                            addItem(to: project)
+                        } label: {
+                            Label("Add new Item", systemImage: "plus")
+                        }
+                    }
+                }
+            }
+          //  .listRowBackground(BlurView(style: .systemUltraThinMaterial))
+        }
+        .listStyle(InsetGroupedListStyle())
+     
+    }
+       
+        
+    
+    
+    var addProjectToolbarItem: some ToolbarContent {
+        
+        ToolbarItem(placement: .navigationBarTrailing) {
+            if showClosedProjects == false {
+                Button(action: addProject) {
+                    Label("Add Project", systemImage: "plus")
+                }
+            }
+        }
+    }
+    
+    var sortOrderToolbarItem: some ToolbarContent {
+        
+        ToolbarItem(placement: .navigationBarLeading) {
+            Button {
+                showingSortOrder.toggle()
+            } label: {
+                Label("Sort", systemImage: "arrow.up.arrow.down")
+            }
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Group {
+                if projects.wrappedValue.isEmpty {
+                    Text("There's nothing here right now")
+                        .foregroundColor(.secondary)
+                } else {
+                    projectsList
+                }
+            }
+            .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
+            .toolbar {
+                addProjectToolbarItem
+                sortOrderToolbarItem
+            }
+            .actionSheet(isPresented: $showingSortOrder) {
+                ActionSheet(title: Text("Sort Items"), message: Text("Sort items"), buttons: [
+                    .default(Text("Optimized")) { sortOrder = .optimized},
+                    .default(Text("Creation Date")) { sortOrder = .creationDate },
+                    .default(Text("Title")) { sortOrder = .title }
+                ])
+            }
+            SelectSomethingView()
+        }
+    }
+    
+    func addProject() {
+        withAnimation {
+            let project = Project(context: managedObjectContext)
+            project.closed = false
+            project.creationDate = Date()
+            dataController.save()
+        }
+    }
+    
+    func addItem(to project: Project) {
+        withAnimation {
+            let item = Item(context: managedObjectContext)
+            item.project = project
+            item.creationDate = Date()
+            dataController.save()
+        }
+    }
+    
+    func delete(_ offsets: IndexSet, from project: Project) {
+        let allItems = project.projectItems(using: sortOrder)
+        for offset in offsets {
+            let item = allItems[offset]
+            dataController.delete(item)
+        }
+        dataController.save()
     }
 }
 
 struct ProjectsView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
     static var previews: some View {
-        ProjectsView()
+        ProjectsView(showClosedProjects: false)
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
     }
 }
+ 
