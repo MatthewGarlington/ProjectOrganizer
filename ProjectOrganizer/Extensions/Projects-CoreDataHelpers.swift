36f8607d373304f31c8644d01a39d63692c6ee58
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 18:22:57 -0400

    feat: The added extension for Project adds the colors array that the user can chose from, the sorted projectItems, and the completionAmount
    The optimized sorting function allows the sorting by the completion amount and priority

--- ProjectOrganizer/Extensions/Projects-CoreDataHelpers.swift
+++ ProjectOrganizer/Extensions/Projects-CoreDataHelpers.swift
@@ -6,3 +6,85 @@
 //
 
 import Foundation
+
+
+
+// This extension hides the optional values in the other views needed to satisfy Core Data
+extension Project {
+    
+    // Adding the colors to be used through the project that are static so they can be referenced
+    
+    static let colors = ["Pink",
+                         "Purple",
+                         "Orange",
+                         "Gold",
+                         "Green",
+                         "Teal",
+                         "Light Blue",
+                         "Dark Blue",
+                         "Midnight",
+                         "Dark Gray",
+                         "Gray"]
+    
+    var projectTitle: String {
+        title ?? NSLocalizedString("New Project", comment: "Create a new project")
+    }
+    var projectDetail: String {
+        detail ?? ""
+    }
+    var projectColor: String {
+        color ?? "Light Blue"
+    }
+    var projectItems: [Item]  {
+        items?.allObjects as? [Item] ?? []
+    }
+    var projectItemsDefaultSorted: [Item] {
+        projectItems.sorted { first, second in
+            if first.completed == false {
+                if second.completed == true {
+                    return true
+                }
+            } else if first.completed == true {
+                if second.completed == false {
+                    return false
+                }
+            }
+            if first.priority > second.priority {
+                return true
+            } else if first.priority < second.priority {
+                return false
+            }
+            return first.itemCreationDate < second.itemCreationDate
+        }
+    }
+    var completionAmount: Double {
+        let originalItems = items?.allObjects as? [Item] ?? []
+        guard originalItems.isEmpty == false else { return 0 }
+        let completedItems = originalItems.filter(\.completed)
+        return Double(completedItems.count) / Double(originalItems.count)
+    }
+    static var example: Project {
+        let controller = DataController.preview
+        let viewContext = controller.container.viewContext
+        let project = Project(context: viewContext)
+        project.title = "Example Project"
+        project.detail = "This is an example project"
+        project.closed = true
+        project.creationDate = Date()
+        
+        return project
+    }
+    func projectItems(using sortOrder: Item.SortOrder) -> [Item] {
+        switch sortOrder {
+        case .title:
+            return projectItems.sorted(by: \Item.itemTitle)
+            
+        case .creationDate:
+            return projectItems.sorted(by: \Item.itemCreationDate)
+            
+        case .optimized:
+            return projectItemsDefaultSorted
+        }
+    }
+}
+
