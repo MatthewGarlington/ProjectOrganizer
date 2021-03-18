c2a43ed307d97805369d56a36ab03d2f4ca32f65
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 18:18:51 -0400

    feat: This added Extension Sorts the items optimized, title, and creationDate through an enum and adds itemTitle, itemDetail, and itemCreationDate

--- ProjectOrganizer/Extensions/Item-CoreDataHelpers.swift
+++ ProjectOrganizer/Extensions/Item-CoreDataHelpers.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+// This extension hides the optional values in the other views needed to satisfy Core Data
+extension Item {
+    enum SortOrder {
+        case optimized, title, creationDate
+    }
+    var itemTitle: String {
+        title ?? NSLocalizedString("New Item", comment: "Add a new New Item")
+    }
+    var itemDetail: String {
+        detail ?? ""
+    }
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+    static var example: Item {
+        let controller = DataController.preview
+        let viewContext = controller.container.viewContext
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+        
+        return item
+    }
+}
