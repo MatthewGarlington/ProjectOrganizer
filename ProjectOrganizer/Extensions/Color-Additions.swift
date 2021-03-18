cd69ec97d246e2be9473a9386b1bdd48893cb694
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 18:01:38 -0400

    style: This modifer adds systemGroupedBackground and secondarySystemGroupedBackground as UIColors to be used throughout the application

--- ProjectOrganizer/Extensions/Color-Additions.swift
+++ ProjectOrganizer/Extensions/Color-Additions.swift
@@ -5,4 +5,11 @@
 //  Created by Matthew Garlington on 3/18/21.
 //
 
-import Foundation
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+
+}
