ec4846858e7423b7a90fdd5679d48632e4cd7473
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-18 17:10:10 -0400

    feat: The added view gives the user the ability to edit a project
    The User is able to enter a name for the project, project color (which will display in the accent color throughout), description of the project, or the ability to close the project. When the project is closed it will move into the Closed Projects tab and the items in that project will not display on the homescreen. The user may also delete a project that prompts the user with a warning to make sure they want to delete the project as it will erase any items in the project as well. The data entered is saved into Core Data and can be saved through the user losing focus of the application or through exiting the navigation link.

--- ProjectOrganizer/Activities/Projects/EditProjectView.swift
+++ ProjectOrganizer/Activities/Projects/EditProjectView.swift
@@ -5,16 +5,102 @@
 //  Created by Matthew Garlington on 3/18/21.
 //
 
+
 import SwiftUI
 
 struct EditProjectView: View {
+    let project: Project
+    
+    
+    @EnvironmentObject var dataController: DataController
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var title: String
+    @State private var detail: String
+    @State private var color: String
+    @State private var showingDeleteConfirm = false
+    
+    
+    // Added to make sure scaling down to smaller devices doesn't make the tappable items too small
+    let colorColums = [
+        GridItem(.adaptive(minimum: 44))
+    ]
+    
+    init(project: Project) {
+        self.project = project
+        _title = State(wrappedValue: project.projectTitle)
+        _detail = State(wrappedValue: project.projectDetail)
+        _color = State(wrappedValue: project.projectColor)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            // section 1 (Basic Settings)
+            Section(header: Text("Basic Settings")) {
+                TextField("Project name", text: $title.onChange(update))
+                TextField("Description of this Project", text: $detail.onChange(update))
+            }
+            // section 2 (Color picker)
+            Section(header: Text("Custom project color")) {
+                LazyVGrid(columns: colorColums) {
+                    ForEach(Project.colors, id: \.self, content: colorButton)
+                }
+                .padding(.vertical)
+            }
+            // section 3
+            Section(footer: Text("Closing a project moves it from the open to closed tab, deleting it removes the project entirely")) {
+                Button(project.closed ? "Reopen this project" : "Close this project") {
+                    project.closed.toggle()
+                }
+                Button("Delete this project") {
+                    showingDeleteConfirm.toggle()
+                }
+                .accentColor(.red)
+            }
+        }
+        .navigationTitle("Edit Project")
+        .onDisappear(perform: dataController.save)
+        .alert(isPresented: $showingDeleteConfirm) {
+            Alert(title: Text("Delete Project?"),
+                  message: Text("Are you sure you want to delete this project? You will also delete all the items within the project it contains."),
+                  primaryButton: .default(Text("Delete"),
+                                          action: delete),
+                  secondaryButton: .cancel())
+        }
+    }
+    
+    func update() {
+        project.title = title
+        project.detail = detail
+        project.color = color
+    }
+    
+    func delete() {
+        dataController.delete(project)
+        presentationMode.wrappedValue.dismiss()
+    }
+    
+    func colorButton(for item: String) -> some View {
+        ZStack {
+            Color(item)
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(6)
+            if item == color {
+                Image(systemName: "checkmark.circle")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+        }
+        .onTapGesture {
+            color = item
+            update()
+        }
     }
 }
 
 struct EditProjectView_Previews: PreviewProvider {
     static var previews: some View {
-        EditProjectView()
+        EditProjectView(project: Project.example)
     }
 }
+
