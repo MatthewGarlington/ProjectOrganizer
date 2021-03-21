4ce63633c2e2e1d88c06f5fffd9c5d0bb7d51ef8
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-21 19:03:59 -0400

    testing: Added to test for assest catelog
    This test ensures that the items are being loaded correctly from the assett catelog

--- ProjectOrganizerTests/AssetTest.swift
+++ ProjectOrganizerTests/AssetTest.swift
@@ -4,29 +4,16 @@
 //
 //  Created by Matthew Garlington on 3/18/21.
 //
-
 import XCTest
+@testable import ProjectOrganizer
 
 class AssetTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testColorsExist() {
+        for color in Project.colors {
+            XCTAssertNotNil(UIColor(named: color), "Failed to load '\(color)' from asset catalog")
         }
     }
-
+    func testJSONLoadsCorrectly() {
+        XCTAssertFalse(Award.allAwards.isEmpty, "Failed to load awards from JSON")
+    }
 }
