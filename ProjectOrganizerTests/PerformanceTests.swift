77c7e6040c7be39b90f97e3e9288091a662722c0
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-22 11:06:38 -0400

    Added Tests for added amounts of data
    Thease Tests were added to ensure that if the user adds alot of data to Core Data, all data will still be prcessed in a reasonable amount of time without slowing down the application

--- ProjectOrganizerTests/PerformanceTests.swift
+++ ProjectOrganizerTests/PerformanceTests.swift
@@ -6,27 +6,22 @@
 //
 
 import XCTest
+@testable import ProjectOrganizer
 
 class PerformanceTests: BaseTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAwardCalculationPerformance() throws {
+        // Create a significant amount of test data
+        for _ in 1...100 {
+        try dataController.createSampleData()
+        }
+        // Simulate a lot of awards to check
+        let awards = Array(repeating: Award.allAwards, count: 25).joined()
+        
+        // Only execute this test if the awards are at this amount 
+        XCTAssertEqual(awards.count, 500, "This checks the number of awards is constant. Change this if you add new awards.")
+        
+        measure {
+            _ = awards.filter(dataController.hasEarned)
         }
     }
-
 }
