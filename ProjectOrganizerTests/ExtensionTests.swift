17cc4154025f79e9aaa4ffd21ad4a979a1831637
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-22 11:05:07 -0400

    testing: Added Tests for awards, Deocdable Strings, and Decodble Dic.
    The tests added ensure that the awards retreived from the JSON file are listed in order upon user selection. The test also ensures that the Decodable strings are read correctly from the JSON files

--- ProjectOrganizerTests/ExtensionTests.swift
+++ ProjectOrganizerTests/ExtensionTests.swift
@@ -5,28 +5,80 @@
 //  Created by Matthew Garlington on 3/18/21.
 //
 
+
 import XCTest
+import SwiftUI
+@testable import ProjectOrganizer
 
-class ExtensionTests: BaseTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class ExtensionTests: XCTestCase {
+    func testSequenceSortingSelf() {
+        let items = [1, 4, 3, 2, 5]
+        let sortedItems = items.sorted(by: \.self)
+        XCTAssertEqual(sortedItems, [1, 2, 3, 4, 5], "The Sorted numbers must be ascending")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testSequenceKeypathSortingCustom() {
+        struct Example: Equatable {
+            let value: String
         }
+        
+        let example1 = Example(value: "a")
+        let example2 = Example(value: "b")
+        let example3 = Example(value: "c")
+        let array = [example1, example2, example3]
+        
+        let sortedItems = array.sorted(by: \.value) {
+            $0 > $1
+        }
+        
+        XCTAssertEqual(sortedItems, [example3, example2, example1], "Reverse sorting should yield c, b, a.")
+        
     }
-
+    
+    func testBundleDecodingAwards() {
+        let awards = Bundle.main.decode([Award].self, from: "Awards.json")
+        XCTAssertFalse(awards.isEmpty, "Awards.json should decode in to a non-empty array.")
+        
+    }
+    
+    func testDecodingString() {
+        let bundle = Bundle(for: ExtensionTests.self)
+        let data = bundle.decode(String.self, from: "DecodableString.json")
+        XCTAssertEqual(data, "The rain in Spain falls mainly on the Spaniards", "The String must match the content of DecodableString.json")
+        
+    }
+    
+    func testDecodingDictionary() {
+        let bundle = Bundle(for: ExtensionTests.self)
+        let data = bundle.decode([String: Int].self, from: "DecodableDictionary.json")
+        XCTAssertEqual(data.count, 3, "There should be three items decoded")
+        XCTAssertEqual(data["One"], 1, "The dictionary should contain Int to String mappings.")
+        
+    }
+    
+    func testBindingOnChange() {
+        // Given
+        var onChangeFunctionRun = false
+        
+        func exampleFunctionToCall() {
+            onChangeFunctionRun = true
+            
+        }
+        
+        var storedValue = ""
+        
+        let binding = Binding(
+            get: { storedValue },
+        set: { storedValue = $0 }
+        
+        )
+        
+        let changedBinding = binding.onChange(exampleFunctionToCall)
+        // When
+        changedBinding.wrappedValue = "Test"
+       
+        // Then
+        XCTAssertTrue(onChangeFunctionRun, "the onChange() function must be run when the binding is changed.")
+    }
 }
+
