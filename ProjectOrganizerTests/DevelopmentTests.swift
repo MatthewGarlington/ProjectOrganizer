759b3913f6a6857be389265cdaa2565f976158f9
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-22 11:02:51 -0400

    testing: Tested example projects, Closing methods, and Deleting projects
    These tests were added to ensure the projects were being deleted. The tests run ensure that the priority of the projects is correct and the example projects are closed when the user issues them to be closed

--- ProjectOrganizerTests/DevelopmentTests.swift
+++ ProjectOrganizerTests/DevelopmentTests.swift
@@ -6,27 +6,36 @@
 //
 
 import XCTest
+import CoreData
+@testable import ProjectOrganizer
 
 class DevelopmentTests: BaseTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testSampleDataCreationWorks() throws {
+        try dataController.createSampleData()
+        
+        XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 5, "There should be 5 sample projects.")
+        XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 50, "There should be 50 sample items.")
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testDeleteAllClearsEverything() throws {
+      //  try dataController.createSampleData()
+        dataController.deleteAll()
+        
+         XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 0, "deleteAll() should leave 0 projects")
+        XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 0, "deleteAll() should leave 0 items")
+        
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testExampleProjectIsClosed() {
+        let project = Project.example
+        XCTAssertTrue(project.closed, "The example project should be closed.")
+        
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testExampleItemIsHighPriority() {
+        let item = Item.example
+        XCTAssertEqual(item.priority, 3, "The example item should be high priority.")
+        
     }
-
 }
