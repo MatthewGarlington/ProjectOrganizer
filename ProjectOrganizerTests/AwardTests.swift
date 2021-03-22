44f6116ce904c34a83dafce1c5a452c360ae8fe4
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-22 08:50:06 -0400

    feat: Added to test for Awards
    The added tests indicate that the awards are being unlocked correctly when the criterion is met, whether items are added to unlock these awards or that items are completed to unlock these awards. A Test is also run to ensure that award images shown match the description for the award presented from the added JSON file

--- ProjectOrganizerTests/AwardTests.swift
+++ ProjectOrganizerTests/AwardTests.swift
@@ -4,29 +4,79 @@
 //
 //  Created by Matthew Garlington on 3/18/21.
 //
-
 import XCTest
+import CoreData
+@testable import ProjectOrganizer
 
 class AwardTests: BaseTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    let awards = Award.allAwards
+    
+    func testAwardIDMatchesName() {
+        for award in awards {
+            XCTAssertEqual(award.id, award.name, "Award ID should always match its name")
+            
         }
+        
+    }
+    
+    func testNewUserHasNoAwards() {
+        
+        for award in awards {
+            
+            XCTAssertFalse(dataController.hasEarned(award: award), "New users should have no earned awards")
+            
+        }
+        
+    }
+    
+    func testAddingItems() {
+        
+        let values = [1, 10, 20, 50, 100, 250, 500, 1000]
+        
+        for (count, value) in values.enumerated() {
+            for _ in 0..<value {
+                _  = Item(context: managedObjectContext)
+            
+            }
+            
+            let matches = awards.filter { award in
+                award.criterion == "items" && dataController.hasEarned(award: award)
+            }
+            
+            XCTAssertEqual(matches.count, count + 1, "Adding \(value) items should unlock \(count + 1)")
+            
+    
+                dataController.deleteAll()
+       
+        }
+        
+    }
+    
+    func testCompletingItems() {
+        
+        let values = [1, 10, 20, 50, 100, 250, 500, 1000]
+        
+        for (count, value) in values.enumerated() {
+
+            for _ in 0..<value {
+               let item = Item(context: managedObjectContext)
+                item.completed = true
+            
+         
+            }
+            
+            let matches = awards.filter { award in
+                award.criterion == "complete" && dataController.hasEarned(award: award)
+            }
+            
+            XCTAssertEqual(matches.count, count + 1, "completing \(value) items should unlock \(count + 1)")
+            
+
+                dataController.deleteAll()
+       
+            
+        }
+        
     }
 
 }
