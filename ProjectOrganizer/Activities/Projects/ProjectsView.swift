ee812fccac06f6ec64e6487b1117a51d458233a7
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-25 16:55:02 -0400

    style: Changed List style
    Changed list to InsetListStyle

--- ProjectOrganizer/Activities/Projects/ProjectsView.swift
+++ ProjectOrganizer/Activities/Projects/ProjectsView.swift
@@ -32,33 +32,38 @@ struct ProjectsView: View {
     var projectsList: some View {
         
     
-      
-//        List {
-        ScrollView {
-        VStack {
+        ZStack {
+ 
+        List {
             ForEach(projects.wrappedValue) { project in
                 Section(header: ProjectHeaderView(project: project)) {
                     ForEach(project.projectItems(using: sortOrder)) { item in
                         ItemRowView(project: project, item: item)
-                        
+                 
                     }
+               
                     // delete the items without messing up the sorted order
                     .onDelete { offsets in
                         delete(offsets, from: project)
                     }
+                    
+                 
                     if showClosedProjects == false {
                         Button {
                             addItem(to: project)
                         } label: {
                             Label("Add new Item", systemImage: "plus")
                         }
+                        
                     }
+                    
                 }
+                .listStyle(InsetListStyle())
+               
             }
-          //  .listRowBackground(BlurView(style: .systemUltraThinMaterial))
+         
         }
         }
-      //  .listStyle(InsetGroupedListStyle())
      
     }
        
@@ -150,3 +155,125 @@ struct ProjectsView_Previews: PreviewProvider {
     }
 }
  
+
+struct ProjectsBackGround: View {
+    var body: some View {
+        ZStack {
+            
+            
+            AngularGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.8472073078, green: 0.4155753255, blue: 0.6355627775, alpha: 1)), Color(#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1))]), center: .center, angle: .degrees(120))
+            
+            LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.1), Color.black.opacity(0.3)]), startPoint: .bottom, endPoint: .top)
+            ZStack {
+                Image("Blob4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .offset(x: -150, y: -400)
+                    .blendMode(.softLight)
+                
+                // Animation Rotates
+                //                    .animation(Animation.easeOut(duration: 80).repeatForever(autoreverses: false))
+                ZStack {
+                    Image("Blob6")
+                        .resizable()
+                        .scaledToFit()
+                        .blendMode(.colorBurn)
+                    Image("Blob6")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150)
+                        .blendMode(.softLight)
+                    Image("Blob6")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250)
+                        .blendMode(.softLight)
+                    
+                    
+                    Image("Blob6")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 350)
+                        .blendMode(.softLight)
+                    
+                    
+                }
+                
+                Image("Blob4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .offset(x: -150, y: -400)
+                    // Added Rotation for the blob
+                    //              .rotationEffect(Angle(degrees:  show ? 360 : 90))
+                    .blendMode(.darken)
+                // Animation Rotates
+                //                    .animation(Animation.easeOut(duration: 100).repeatForever(autoreverses: false))
+                //
+                
+                Image("Blob4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .offset(x: -150, y: -400)
+                    // Added Rotation for the blob
+                    //              .rotationEffect(Angle(degrees:  show ? 360 : 90))
+                    .blendMode(.darken)
+                // Animation Rotates
+                //                    .animation(Animation.easeOut(duration: 120).repeatForever(autoreverses: false))
+                
+                Image("Blob4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .offset(x: -150, y: -400)
+                    // Added Rotation for the blob
+                    //              .rotationEffect(Angle(degrees:  show ? 360 : 90))
+                    .blendMode(.darken)
+                // Animation Rotates
+                //                    .animation(Animation.easeOut(duration: 140).repeatForever(autoreverses: false))
+            }
+            ZStack {
+                
+                
+                Image("Blob4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .offset(x: -150, y: -400)
+                    // Added Rotation for the blob
+                    //              .rotationEffect(Angle(degrees:  show ? 360 : 90))
+                    .blendMode(.softLight)
+                // Animation Rotates
+                //                    .animation(Animation.easeOut(duration: 100).repeatForever(autoreverses: false))
+                
+                
+                Image("Blob4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 350, height: 350)
+                    .offset(x: -150, y: -400)
+                    // Added Rotation for the blob
+                    //              .rotationEffect(Angle(degrees:  show ? 360 : 90))
+                    .blendMode(.softLight)
+                //                    // Animation Rotates
+                //                    .animation(Animation.easeOut(duration: 120).repeatForever(autoreverses: false))
+                
+                Image("Blob4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 400, height: 400)
+                    .offset(x: -150, y: -400)
+                    // Added Rotation for the blob
+                    //                    .rotationEffect(Angle(degrees:  show ? 360 : 90))
+                    .blendMode(.softLight)
+                // Animation Rotates
+                //                    .animation(Animation.linear(duration: 140).repeatForever(autoreverses: false))
+                //
+                
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
