e67771f6e42f91f434f42cec6ab3648d35e38b13
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-01 12:51:56 -0400

    testing: Added UI Test testAddingItemInsertsRows
    The added UI Tests ensures that when a project is added there will be that number of projects showing and when a project is added, Items for that project are added as well

--- ProjectOrganizerUITests/ProjectOrganizerUITests.swift
+++ ProjectOrganizerUITests/ProjectOrganizerUITests.swift
@@ -24,4 +24,25 @@ class ProjectOrganizerUITests: XCTestCase {
         XCTAssertEqual(app.tabBars.buttons.count, 4, "There should be 4 Tabs in the App")
 
     }
+    
+    func testOpenTabAddsProjects() {
+        app.buttons["Open"].tap()
+        XCTAssertEqual(app.tables.cells.count, 0, "There should be no list rows initially.")
+        
+        for tapCount in 1...5 {
+            app.buttons["add"].tap()
+            XCTAssertEqual(app.tables.cells.count, tapCount, "There should be \(tapCount) list row(s) initially")
+        }
+    }
+    
+    func testAddingItemInsertsRows() {
+        app.buttons["Open"].tap()
+        XCTAssertEqual(app.tables.cells.count, 0, "There should be no list rows initially.")
+        
+        app.buttons["add"].tap()
+        XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 list row after adding a project")
+        
+        app.buttons["Add new Item"].tap()
+        XCTAssertEqual(app.tables.cells.count, 2, "There should be 2 list row after adding a Items")
+    }
 }
