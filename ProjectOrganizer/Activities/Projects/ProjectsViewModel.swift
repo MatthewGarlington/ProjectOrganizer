4a66ac42dc1ae88ea6f485d5b17c6b4758662e9b
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-02 14:32:40 -0400

    Added ProjectViewmodel

---
+++ ProjectOrganizer/Activities/Projects/ProjectsViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectsViewModel.swift
+//  ProjectOrganizer
+//
+//  Created by Matthew Garlington on 4/2/21.
+//
+
+import SwiftUI
+
+struct ProjectsViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectsViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectsViewModel()
+    }
+}
