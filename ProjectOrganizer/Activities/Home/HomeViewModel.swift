5a510e425ff747342c265ea36c2ebc369d0a6923
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-03 07:47:42 -0400

    Added HomeViewModel and Added ItemRowViewModel

---
+++ ProjectOrganizer/Activities/Home/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewViewModel.swift
+//  ProjectOrganizer
+//
+//  Created by Matthew Garlington on 4/2/21.
+//
+
+import SwiftUI
+
+struct HomeViewViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewViewModel()
+    }
+}
---
+++ ProjectOrganizer/Activities/Projects/ItemRowViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemRowViewModel.swift
+//  ProjectOrganizer
+//
+//  Created by Matthew Garlington on 4/3/21.
+//
+
+import Foundation
