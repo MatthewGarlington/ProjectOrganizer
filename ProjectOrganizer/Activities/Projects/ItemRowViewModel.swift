12c6c250dfbf244419f2d4a0260e88f7380a1f5c
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-03 07:52:24 -0400

    refactor: refactored to MVVM architecture
    Refactored ItemRow to pull out controllers into seperate viewmodel. This was done by seperating the icon and color from the the View and conforming to the Observable Object Protocol in order for the ItemRowView to Observe the changes made. This refacotring helps conform to the MVVM architecture and keeps the UI elements in the view while placing the controllers in a seperate file for easier access and understanding of how the Item row is being manipulated by the user as well as the UI elements

--- ProjectOrganizer/Activities/Projects/ItemRowViewModel.swift
+++ ProjectOrganizer/Activities/Projects/ItemRowViewModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+extension ItemRowView {
+    class ViewModel: ObservableObject {
+       
+        let project: Project
+        let item: Item
+        
+        var title: String {
+            item.itemTitle
+        }
+        
+        var icon: String {
+            
+            if item.completed {
+                return "checkmark.circle"
+            } else if item.priority == 3 {
+                return  "exclamationmark.triangle"
+            } else {
+                return "checkmark.circle"
+            }
+        }
+        var color: String? {
+            
+            if item.completed {
+                return project.projectColor
+            } else if item.priority == 3 {
+                return project.projectColor
+            } else {
+                return nil
+            }
+        }
+         init(project: Project, item: Item) {
+            self.project = project
+            self.item = item
+        }
+        
+        
+        
+    }
+}
