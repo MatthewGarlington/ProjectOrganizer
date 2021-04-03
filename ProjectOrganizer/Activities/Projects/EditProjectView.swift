67e4c830d2c6e1bd01306fc72c70368db607a63d
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-03 13:31:27 -0400

    feat: Added Custom Haptic Feedback to Close Projects Button
    The added Haptic feedback triggers a sharp and slow diminshing haptic found in the toggleClosed function created in EditProjectsView using a CHHaptic event from the imported CoreHaptics into EditProjectView. This added haptic feedback will help give the feeling of novelty when the user concludes a project

--- ProjectOrganizer/Activities/Projects/EditProjectView.swift
+++ ProjectOrganizer/Activities/Projects/EditProjectView.swift
@@ -7,10 +7,10 @@
 
 
 import SwiftUI
+import CoreHaptics
 
 struct EditProjectView: View {
     @ObservedObject var project: Project
-    
     
     @EnvironmentObject var dataController: DataController
     @Environment(\.presentationMode) var presentationMode
@@ -19,6 +19,8 @@ struct EditProjectView: View {
     @State private var detail: String
     @State private var color: String
     @State private var showingDeleteConfirm = false
+
+    @State private var engine = try? CHHapticEngine()
     
     
     // Added to make sure scaling down to smaller devices doesn't make the tappable items too small
@@ -49,9 +51,7 @@ struct EditProjectView: View {
             }
             // section 3
             Section(footer: Text("Closing a project moves it from the open to closed tab, deleting it removes the project entirely")) {
-                Button(project.closed ? "Reopen this project" : "Close this project") {
-                    project.closed.toggle()
-                }
+                Button(project.closed ? "Reopen this project" : "Close this project", action: toggleClosed)
                 Button("Delete this project") {
                     showingDeleteConfirm.toggle()
                 }
@@ -78,6 +78,54 @@ struct EditProjectView: View {
     func delete() {
         dataController.delete(project)
         presentationMode.wrappedValue.dismiss()
+    }
+
+    func toggleClosed() {
+            project.closed.toggle()
+
+            if project.closed {
+               // Trigger Custom Haptics
+                do {
+                    try? engine?.start()
+
+                    let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                    let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                    let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                    let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                    let parameter = CHHapticParameterCurve(
+                        parameterID: .hapticIntensityControl,
+                        controlPoints: [start, end],
+                        relativeTime: 0
+                    )
+                    // Play the Haptic that was set up above
+
+                    let event1 = CHHapticEvent(
+                        eventType: .hapticTransient,
+                        parameters: [intensity, sharpness],
+                        relativeTime: 0
+                    )
+
+                    let event2 = CHHapticEvent(
+                        eventType: .hapticContinuous,
+                        parameters: [intensity, sharpness],
+                        relativeTime: 0.125,
+                        duration: 1
+                    )
+
+                    let pattern = try CHHapticPattern(events: [event1, event2],
+                                                      parameterCurves: [parameter]
+                    )
+
+                    let player = try engine?.makePlayer(with: pattern)
+                    try player?.start(atTime: 0)
+                } catch {
+
+                    // playing haptics didn't work
+                }
+
+        }
     }
     
     func colorButton(for item: String) -> some View {
