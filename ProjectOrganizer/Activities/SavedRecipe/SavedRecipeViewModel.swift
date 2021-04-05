ca81d5c9e149fea98cd2ec17a375fb38ccf7cedb
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-05 08:38:00 -0400

    feat: Added View Model
    The added view model acts to add the Saved Recipe to Core Data in order to make the Recipes persistant and uneffected by the Shopping List and Purchased Tabs. This was done in case the user wants to save a recipe to add back to the shopping list at a later time

--- ProjectOrganizer/Activities/SavedRecipe/SavedRecipeViewModel.swift
+++ ProjectOrganizer/Activities/SavedRecipe/SavedRecipeViewModel.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+import CoreData
+import SwiftUI
+
+extension SavedRecipeView {
+    class ViewModel: NSObject, ObservableObject, NSFetchedResultsControllerDelegate {
+        let dataController: DataController
+
+        var sortOrder = Item.SortOrder.optimized
+        let showSavedProjects: Bool
+
+        private let projectController: NSFetchedResultsController<Project>
+        @Published var projects = [Project]()
+
+        init(dataController: DataController, showSavedProjects: Bool) {
+            self.dataController = dataController
+            self.showSavedProjects = showSavedProjects
+
+            // Used to ensure MVVM where the request is
+            // able to be accessed by other Views
+            let request: NSFetchRequest<Project> = Project.fetchRequest()
+            request.sortDescriptors = [NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)]
+            request.predicate = NSPredicate(format: "saved = %d", showSavedProjects)
+
+            projectController = NSFetchedResultsController(fetchRequest: request,
+                                                           managedObjectContext: dataController.container.viewContext,
+                                                           sectionNameKeyPath: nil,
+                                                           cacheName: nil
+            )
+
+            super.init()
+            projectController.delegate = self
+
+            do {
+                try projectController.performFetch()
+                projects = projectController.fetchedObjects ?? []
+
+            } catch {
+                print("Failed to fetch our Recipes")
+            }
+
+        }
+        func addProject() {
+                let project = Project(context: dataController.container.viewContext)
+                project.closed = false
+                project.creationDate = Date()
+                dataController.save()
+        }
+
+        func addItem(to project: Project) {
+                let item = Item(context: dataController.container.viewContext)
+                item.project = project
+                item.creationDate = Date()
+                dataController.save()
+        }
+
+        func delete(_ offsets: IndexSet, from project: Project) {
+            let allItems = project.projectItems(using: sortOrder)
+            for offset in offsets {
+                let item = allItems[offset]
+                dataController.delete(item)
+            }
+            dataController.save()
+        }
+
+        func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+            if let newProjects = controller.fetchedObjects as? [Project] {
+                projects = newProjects
+            }
+        }
+
+    }
+}
+
