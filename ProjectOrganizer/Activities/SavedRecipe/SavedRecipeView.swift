e483c61f8b5baadd338c41dd9d307c3942dae153
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-05 08:35:23 -0400

    feat: Added SavedRecipeView
    The added view presents Recipes and ingredients to be saved in this view if the user selects to save the recipe in the EditProjectView. This data will be persitent through Core Data when the app is closed.

--- ProjectOrganizer/Activities/SavedRecipe/SavedRecipeView.swift
+++ ProjectOrganizer/Activities/SavedRecipe/SavedRecipeView.swift
@@ -8,13 +8,86 @@
 import SwiftUI
 
 struct SavedRecipeView: View {
+    static let savedTag: String? = "Saved Recipes"
+
+    @StateObject var viewModel: ViewModel
+    @State private var showingSortOrder = false
+
+
+
+    var savedProjectsList: some View {
+
+
+        ZStack {
+
+            VStack {
+                List {
+                ForEach(viewModel.projects) { project in
+                    Section(header: SavedRecipeHeaderView(project: project)) {
+                        ForEach(project.projectItems(using: viewModel.sortOrder)) { item in
+                            ItemRowView(project: project, item: item)
+
+                        }
+
+                        // delete the items without messing up the sorted order
+                        .onDelete { offsets in
+                            viewModel.delete(offsets, from: project)
+                        }
+
+
+                    }
+                    .listStyle(InsetListStyle())
+
+                }
+
+                }
+            }
+        }
+
+    }
+
+
+
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Group {
+                if viewModel.projects.isEmpty {
+                    Text("There's nothing here right now")
+                        .foregroundColor(.secondary)
+                } else {
+
+                    savedProjectsList
+                }
+            }
+
+
+            .navigationBarTitle("Saved Recipes")
+
+
+            .actionSheet(isPresented: $showingSortOrder) {
+                ActionSheet(title: Text("Sort Items"), message: Text("Sort items"), buttons: [
+                    .default(Text("Optimized")) { viewModel.sortOrder = .optimized},
+                    .default(Text("Creation Date")) { viewModel.sortOrder = .creationDate },
+                    .default(Text("Title")) { viewModel.sortOrder = .title }
+                ])
+            }
+            SelectSomethingView()
+        }
+    }
+
+    init(dataController: DataController, showSavedProjects: Bool) {
+        let viewModel = ViewModel(dataController: dataController, showSavedProjects: showSavedProjects)
+        _viewModel = StateObject(wrappedValue: viewModel)
     }
 }
 
 struct SavedRecipeView_Previews: PreviewProvider {
+
     static var previews: some View {
-        SavedRecipeView()
+        SavedRecipeView(dataController: DataController.preview, showSavedProjects: false)
     }
 }
+
+
