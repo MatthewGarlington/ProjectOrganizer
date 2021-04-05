d4a452c06d18b34e2462e85d03becbd98b997077
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-05 07:40:35 -0400

    Added EditSavedRecipeView and SavedRecipeHeaderView

---
+++ ProjectOrganizer/Activities/SavedRecipe/EditSavedRecipeView.swift
@@ -0,0 +1,20 @@
+//
+//  EditSavedRecipeView.swift
+//  ProjectOrganizer
+//
+//  Created by Matthew Garlington on 4/4/21.
+//
+
+import SwiftUI
+
+struct EditSavedRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditSavedRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditSavedRecipeView()
+    }
+}
---
+++ ProjectOrganizer/Activities/SavedRecipe/SavedRecipeHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  SavedRecipeHeaderView.swift
+//  ProjectOrganizer
+//
+//  Created by Matthew Garlington on 4/4/21.
+//
+
+import SwiftUI
+
+struct SavedRecipeHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SavedRecipeHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SavedRecipeHeaderView()
+    }
+}
