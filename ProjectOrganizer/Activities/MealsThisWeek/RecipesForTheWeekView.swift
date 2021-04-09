6fc9303c9283e7426374b452e0b494963b7eaf6b
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-09 12:42:39 -0400

    feat: Added RecipesForTheWeekView
    This view displays displays recipes in a scrollview for each day of the week where the user can organize the weekly meals and recipes need to be added to the shopping list

--- ProjectOrganizer/Activities/MealsThisWeek/RecipesForTheWeekView.swift
+++ ProjectOrganizer/Activities/MealsThisWeek/RecipesForTheWeekView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct RecipesForTheWeekView: View {
+    let meal = ["Breakfast", "Lunch" ,"Dinner"]
+    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Meals for the Week")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+            ScrollView {
+                VStack {
+                    ForEach(weekDays, id: \.self) { num in
+                        Text(num)
+                            .bold()
+                            .font(.title)
+                            .underline()
+                        Spacer()
+                            .frame(width: 350, height: 100)
+                            .background(Color.black.opacity(0.2))
+                            .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous), style: FillStyle())
+                            .overlay(
+                                VStack(alignment: .leading) {
+                                    HStack {
+                                        Text("Breakfast:")
+                                            .bold()
+                                        Text("Meal \(num)")
+                                    }
+
+                                    HStack {
+                                        Text("Lunch:")
+                                            .bold()
+                                        Text("Meal \(num)")
+                                    }
+
+                                    HStack {
+                                        Text("Dinner:")
+                                            .bold()
+                                        Text("Meal \(num)")
+                                    }
+                                }
+                            )
+                            .padding()
+                    }
+                }
+            }
+        }
     }
 }
 
