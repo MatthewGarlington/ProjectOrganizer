2b7a45d6ee27793b176185e9855a1791699c66fb
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-09 17:49:30 -0400

    feat: Added view to Present Meals broken down by day of the week
    The added view currently has a top section for not assigned meals that have been triggered by the button in EditProjectView that usees the title of the project to organize during the week and whether for breakfast, lunch, or dinner these meals are to be had

--- ProjectOrganizer/Activities/MealsThisWeek/RecipesForTheWeekView.swift
+++ ProjectOrganizer/Activities/MealsThisWeek/RecipesForTheWeekView.swift
@@ -7,55 +7,94 @@
 
 import SwiftUI
 
+
 struct RecipesForTheWeekView: View {
-    let meal = ["Breakfast", "Lunch" ,"Dinner"]
-    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    var body: some View {
+    static let weekList: String? = "Meals This Week"
+
+    @StateObject var viewModel: ViewModel
+    @State private var showingSortOrder = false
+
+    let noMealShown = "No Meal Shown For This Day"
+    let weekDays = [
+                           "Sunday",
+                           "Monday",
+                           "Tuesday",
+                           "Wednesday",
+                           "Thursday",
+                           "Friday",
+                           "Saturday"]
+    let meals = [ "Breakfast", "Lunch", "Dinner"
+    ]
+
+
+
+    var weekMealsList: some View {
+
+
+
         VStack {
-            Text("Meals for the Week")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-            ScrollView {
-                VStack {
-                    ForEach(weekDays, id: \.self) { num in
-                        Text(num)
-                            .bold()
-                            .font(.title)
-                            .underline()
-                        Spacer()
-                            .frame(width: 350, height: 100)
-                            .background(Color.black.opacity(0.2))
-                            .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous), style: FillStyle())
-                            .overlay(
-                                VStack(alignment: .leading) {
-                                    HStack {
-                                        Text("Breakfast:")
-                                            .bold()
-                                        Text("Meal \(num)")
-                                    }
 
-                                    HStack {
-                                        Text("Lunch:")
-                                            .bold()
-                                        Text("Meal \(num)")
-                                    }
+            List {
 
-                                    HStack {
-                                        Text("Dinner:")
-                                            .bold()
-                                        Text("Meal \(num)")
-                                    }
-                                }
-                            )
-                            .padding()
+
+                    Section(header: Text("Not Assigned Yet")) {
+                        ForEach(viewModel.projects) { project in
+                        NavigationLink(
+                            destination: EditRecipeForThisWeek(project: project),
+                            label: {
+                                Text("\(project.title ?? "")")
+                            })
+
+                    }
+                }
+                ForEach(weekDays.indices, id: \.self) { num in
+                    Section(header: Text("\(weekDays[num])")) {
+                        ForEach(meals.indices, id: \.self) { meal in
+                            Section(header: Text("\(meals[meal])")) {
+                            }
+
+                        }
                     }
                 }
             }
         }
+
+    }
+
+
+
+
+
+    var body: some View {
+        NavigationView {
+            Group {
+                if viewModel.projects.isEmpty {
+                    Text("There's nothing here right now")
+                        .foregroundColor(.secondary)
+                } else {
+
+                    weekMealsList
+                }
+            }
+
+
+            .navigationBarTitle("Meals This Week")
+
+
+            SelectSomethingView()
+        }
+    }
+
+    init(dataController: DataController, mealsThisWeek: Bool) {
+        let viewModel = ViewModel(dataController: dataController, mealsThisWeek: mealsThisWeek)
+        _viewModel = StateObject(wrappedValue: viewModel)
     }
 }
 
 struct RecipesForTheWeekView_Previews: PreviewProvider {
+
     static var previews: some View {
-        RecipesForTheWeekView()
+        RecipesForTheWeekView(dataController: DataController.preview, mealsThisWeek: false)
     }
 }
+
