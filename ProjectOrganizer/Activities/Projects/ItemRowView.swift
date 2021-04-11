8c5679f36b6eaeefb43ad101cce556ae434d75d5
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-11 12:52:17 -0400

    feat: Added Haptic feedback for Item completion
    The added haptics are set to a custom vibration using CoreHaptics upon completeion and system haptics of UIImpactFeedbackGenerator on toggling the item to not completed. This gives the user feedback that the system image leading in the HStack of the list is a button that can be toggled to complete the item

--- ProjectOrganizer/Activities/Projects/ItemRowView.swift
+++ ProjectOrganizer/Activities/Projects/ItemRowView.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ItemRowView: View {
     @StateObject var viewModel: ViewModel
     @ObservedObject var item: Item
     @State private var offset: CGSize = .zero
+    @State private var engine = try? CHHapticEngine()
+    
   
     
     var body: some View {
@@ -22,14 +25,75 @@ struct ItemRowView: View {
                 } icon: {
                     Image(systemName: viewModel.icon)
                         .foregroundColor(viewModel.color.map { Color($0) } ?? .clear)
+
+                        .onTapGesture {
+                            toggleHomeItem()
+
+                            func toggleHomeItem() {
+                                item.completed.toggle()
+
+
+                                if item.completed {
+
+                                    
+                                
+                                       // Trigger Custom Haptics
+                                        do {
+                                            try? engine?.start()
+
+                                            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                                            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                                            let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                                            let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                                            let parameter = CHHapticParameterCurve(
+                                                parameterID: .hapticIntensityControl,
+                                                controlPoints: [start, end],
+                                                relativeTime: 0
+                                            )
+                                            // Play the Haptic that was set up above
+
+                                            let event1 = CHHapticEvent(
+                                                eventType: .hapticTransient,
+                                                parameters: [intensity, sharpness],
+                                                relativeTime: 0
+                                            )
+
+                                            let event2 = CHHapticEvent(
+                                                eventType: .hapticContinuous,
+                                                parameters: [intensity, sharpness],
+                                                relativeTime: 0.125,
+                                                duration: 0.15
+                                            )
+
+                                            let pattern = try CHHapticPattern(events: [event1, event2],
+                                                                              parameterCurves: [parameter]
+                                            )
+
+                                            let player = try engine?.makePlayer(with: pattern)
+                                            try player?.start(atTime: 0)
+                                        } catch {
+
+                                            // playing haptics didn't work
+                                        }
+
+                                    } else {
+                                        item.completed = false
+
+                                        let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+                                             impactMed.impactOccurred()
+                                    }
+                            }
+                        }
+
                 }
+
                 .padding()
             }
 
         }
-        .onLongPressGesture {
-            item.completed.toggle()
-        }
+
     }
     
     init(project: Project, item: Item) {
