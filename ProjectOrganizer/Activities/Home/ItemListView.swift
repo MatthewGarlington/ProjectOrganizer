0a488f97c1c151feabb41a2664c976191ea2d1d1
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-11 12:55:21 -0400

    Added haptics for completion of items in Home and Project view

--- ProjectOrganizer/Activities/Home/ItemListView.swift
+++ ProjectOrganizer/Activities/Home/ItemListView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ItemListView: View {
     let title: LocalizedStringKey
@@ -16,6 +17,7 @@ struct ItemListView: View {
     
 
     @State private var completeItem = false
+    @State private var engine = try? CHHapticEngine()
     
     var body: some View {
         if items.isEmpty {
@@ -29,7 +31,55 @@ struct ItemListView: View {
                 NavigationLink(destination: EditItemView(item: item)) {
                     HStack(spacing: 20) {
                         Button(action: {
-                            item.completed.toggle()
+                            toggleHomeItem()
+
+                            func toggleHomeItem() {
+                                item.completed = true
+
+                                    if item.completed {
+                                       // Trigger Custom Haptics
+                                        do {
+                                            try? engine?.start()
+
+                                            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                                            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                                            let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                                            let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                                            let parameter = CHHapticParameterCurve(
+                                                parameterID: .hapticIntensityControl,
+                                                controlPoints: [start, end],
+                                                relativeTime: 0
+                                            )
+                                            // Play the Haptic that was set up above
+
+                                            let event1 = CHHapticEvent(
+                                                eventType: .hapticTransient,
+                                                parameters: [intensity, sharpness],
+                                                relativeTime: 0
+                                            )
+
+                                            let event2 = CHHapticEvent(
+                                                eventType: .hapticContinuous,
+                                                parameters: [intensity, sharpness],
+                                                relativeTime: 0.125,
+                                                duration: 0.15
+                                            )
+
+                                            let pattern = try CHHapticPattern(events: [event1, event2],
+                                                                              parameterCurves: [parameter]
+                                            )
+
+                                            let player = try engine?.makePlayer(with: pattern)
+                                            try player?.start(atTime: 0)
+                                        } catch {
+
+                                            // playing haptics didn't work
+                                        }
+
+                                }
+                            }
                         }, label: {
                         Circle()
                             .stroke(Color(item.project?.projectColor ?? "Light Blue"), lineWidth: 3)
@@ -60,9 +110,14 @@ struct ItemListView: View {
                             .stroke(LinearGradient(gradient: Gradient(colors: [Color.white, Color.green.opacity(0.4)]), startPoint: .leading, endPoint: .trailing), lineWidth: 2)
                      )
                 }
+
             }
+
+            
         }
     }
+
+
 }
 
 //struct ItemListView_Previews: PreviewProvider {
