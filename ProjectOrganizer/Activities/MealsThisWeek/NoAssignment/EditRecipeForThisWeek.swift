e860b17cc71272f4078deadebcc2d0067822f3a8
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-11 12:54:33 -0400

    testing: No Changes made

--- ProjectOrganizer/Activities/MealsThisWeek/NoAssignment/EditRecipeForThisWeek.swift
+++ ProjectOrganizer/Activities/MealsThisWeek/NoAssignment/EditRecipeForThisWeek.swift
@@ -161,53 +161,6 @@ struct EditRecipeForThisWeek: View {
         presentationMode.wrappedValue.dismiss()
     }
 
-    func toggleClosed() {
-            project.closed = false
-
-            if project.closed {
-               // Trigger Custom Haptics
-                do {
-                    try? engine?.start()
-
-                    let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
-                    let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-
-                    let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
-                    let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
-
-                    let parameter = CHHapticParameterCurve(
-                        parameterID: .hapticIntensityControl,
-                        controlPoints: [start, end],
-                        relativeTime: 0
-                    )
-                    // Play the Haptic that was set up above
-
-                    let event1 = CHHapticEvent(
-                        eventType: .hapticTransient,
-                        parameters: [intensity, sharpness],
-                        relativeTime: 0
-                    )
-
-                    let event2 = CHHapticEvent(
-                        eventType: .hapticContinuous,
-                        parameters: [intensity, sharpness],
-                        relativeTime: 0.125,
-                        duration: 1
-                    )
-
-                    let pattern = try CHHapticPattern(events: [event1, event2],
-                                                      parameterCurves: [parameter]
-                    )
-
-                    let player = try engine?.makePlayer(with: pattern)
-                    try player?.start(atTime: 0)
-                } catch {
-
-                    // playing haptics didn't work
-                }
-
-        }
-    }
 
     func colorButton(for item: String) -> some View {
         ZStack {
