6cdd4b27fd26c39b4d7aedba984293b8f8f0c8a7
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-12 17:11:24 -0400

    style: Removed Item Icons from the SavedItemRow
    This was done by returning nil on all colors in the SavedItemViewModel. This presents the title in SavedItemRow without an icon

--- ProjectOrganizer/Activities/SavedRecipe/SavedItemRowViewModel.swift
+++ ProjectOrganizer/Activities/SavedRecipe/SavedItemRowViewModel.swift
@@ -5,16 +5,46 @@
 //  Created by Matthew Garlington on 4/11/21.
 //
 
-import SwiftUI
+import Foundation
 
-struct SavedItemRowViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+extension SavedItemRow {
+    class ViewModel: ObservableObject {
+
+        let project: Project
+        let item: Item
+
+        var title: String {
+            item.itemTitle
+        }
+
+        var icon: String {
+
+            if item.completed {
+                return "checkmark.circle"
+            } else if item.priority == 3 {
+                return  "exclamationmark.triangle"
+            } else {
+                return "circle"
+            }
+        }
+        var color: String? {
+
+            if item.completed {
+                return nil
+            } else if item.priority == 3 {
+                return nil
+            } else {
+                return nil
+            }
+        }
+         init(project: Project, item: Item) {
+            self.project = project
+            self.item = item
+        }
+
+
+
     }
 }
 
-struct SavedItemRowViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        SavedItemRowViewModel()
-    }
-}
