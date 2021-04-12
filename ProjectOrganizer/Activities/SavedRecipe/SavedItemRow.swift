2dfb3b583d1b6fa87b6ade4292d48cfb09f78302
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-12 17:15:20 -0400

    feat: Removed Icon from List

--- ProjectOrganizer/Activities/SavedRecipe/SavedItemRow.swift
+++ ProjectOrganizer/Activities/SavedRecipe/SavedItemRow.swift
@@ -5,16 +5,46 @@
 //  Created by Matthew Garlington on 4/11/21.
 //
 
+
 import SwiftUI
+import CoreHaptics
 
 struct SavedItemRow: View {
+    @StateObject var viewModel: ViewModel
+    @ObservedObject var item: Item
+    @State private var offset: CGSize = .zero
+    @State private var engine = try? CHHapticEngine()
+
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: EditItemView(item: item)) {
+            ZStack(alignment: .leading) {
+                Label {
+                    Text(item.itemTitle)
+
+                } icon: {
+                    Image(systemName: viewModel.icon)
+                        .foregroundColor(viewModel.color.map { Color($0) } ?? .clear)
+                }
+                .padding()
+            }
+
+        }
+
+    }
+
+    init(project: Project, item: Item) {
+        let viewModel = ViewModel(project: project, item: item)
+        _viewModel = StateObject(wrappedValue: viewModel)
+
+        self.item = item
     }
 }
 
 struct SavedItemRow_Previews: PreviewProvider {
     static var previews: some View {
-        SavedItemRow()
+        SavedItemRow(project: Project.example, item: Item.example)
     }
 }
+
