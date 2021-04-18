11994152853b356a5ab4644669d3284bcc413904
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-18 15:08:45 -0400

    style: Added animation to check off button
    The added animation has two parts: On press of the icon or label that completes the item with a custom feedback generation for complete and unchecked. The icon displays a ring around the cicle and the circle shinks as the user continues to press until the item is completed upon release. The item title was placed in a HStack with an amount placed in perentheses that can be added in the project edit view

--- ProjectOrganizer/Activities/Projects/ItemRowView.swift
+++ ProjectOrganizer/Activities/Projects/ItemRowView.swift
@@ -13,86 +13,249 @@ struct ItemRowView: View {
     @ObservedObject var item: Item
     @State private var offset: CGSize = .zero
     @State private var engine = try? CHHapticEngine()
+    @State private var icon = "checkmark.circle.fill"
+    @State private var colorStroke = Color.green
+    @State private var press = false
+    @State private var animationStroke: CGFloat = 1
+    @State private var explodeStroke: CGFloat = 1
+    @GestureState var tap = false
     
   
     
     var body: some View {
+        let colorItem = viewModel.color.map { Color($0) } ?? .clear
+
         NavigationLink(destination: EditItemView(item: item)) {
-            ZStack(alignment: .leading) {
+            ZStack(alignment: .center) {
                 Label {
-                    Text(item.itemTitle)
-                    
+                    HStack {
+                        
+                        Text(item.itemTitle)
+                        Text("(\(item.itemAmount))")
+
+
+                    }
+                    .foregroundColor(colorItem)
+                    .padding()
                 } icon: {
                     Image(systemName: viewModel.icon)
-                        .foregroundColor(viewModel.color.map { Color($0) } ?? .clear)
+                        .foregroundColor(colorItem)
+                        .font(.system(size: 20))
+                        .frame(width: 50, height: 50)
 
-                        .onTapGesture {
-                            toggleHomeItem()
+                        .background(
+                            ZStack {
+                                Circle()
+                                    .foregroundColor(.clear)
+                                    .background(BlurView(style: item.completed ? UIBlurEffect.Style.systemThickMaterial :UIBlurEffect.Style.systemUltraThinMaterial ))
 
-                            func toggleHomeItem() {
-                                item.completed.toggle()
-
-
-                                if item.completed {
-
-                                    
-                                
-                                       // Trigger Custom Haptics
-                                        do {
-                                            try? engine?.start()
-
-                                            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
-                                            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-
-                                            let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
-                                            let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
-
-                                            let parameter = CHHapticParameterCurve(
-                                                parameterID: .hapticIntensityControl,
-                                                controlPoints: [start, end],
-                                                relativeTime: 0
-                                            )
-                                            // Play the Haptic that was set up above
-
-                                            let event1 = CHHapticEvent(
-                                                eventType: .hapticTransient,
-                                                parameters: [intensity, sharpness],
-                                                relativeTime: 0
-                                            )
-
-                                            let event2 = CHHapticEvent(
-                                                eventType: .hapticContinuous,
-                                                parameters: [intensity, sharpness],
-                                                relativeTime: 0.125,
-                                                duration: 0.15
-                                            )
-
-                                            let pattern = try CHHapticPattern(events: [event1, event2],
-                                                                              parameterCurves: [parameter]
-                                            )
-
-                                            let player = try engine?.makePlayer(with: pattern)
-                                            try player?.start(atTime: 0)
-                                        } catch {
-
-                                            // playing haptics didn't work
-                                        }
-
-                                    } else {
-                                        item.completed = false
-
-                                        let impactMed = UIImpactFeedbackGenerator(style: .heavy)
-                                             impactMed.impactOccurred()
-                                    }
                             }
-                        }
+
+                        )
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle()
+                                .trim(from: press ? 0.001 : 1, to: 1)
+                                .stroke(LinearGradient(gradient: Gradient(colors: [((colorItem)), ((colorItem))]), startPoint: .topLeading, endPoint: .topTrailing), style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                                .frame(width: 50, height: 50)
+                                .rotationEffect(Angle(degrees: 90))
+                                .rotation3DEffect(Angle(degrees: 180),
+                                    axis: (x: 1, y: 0, z: 0))
+                               // .shadow(color: ((colorStroke)).opacity(0.3), radius: 5, x: 3, y: 3)
+                                .animation(.easeInOut)
+
+                        )
+
+
+
+                        .overlay(
+                            Circle()
+                                .stroke((colorItem), lineWidth: 5)
+                                .scaleEffect(tap ? 0 : animationStroke)
+                                .opacity(Double(2 - animationStroke))
+                                .animation(
+                                         Animation.easeOut(duration: 1)
+
+                                     )
+
+                        )
+                //        .onTapGesture {
+                //
+                //            self.animationStroke = 2
+                //        }
+
+                        .overlay(
+                            Circle()
+                                .stroke((colorItem), lineWidth: 3)
+                                .scaleEffect(tap ? 0 : animationStroke)
+                                .opacity(Double(2 - animationStroke))
+                                .animation(
+                                         Animation.easeOut(duration: 2)
+
+                                     )
+
+                        )
+
+
+                        .overlay(
+                            Circle()
+                                .stroke((colorItem), lineWidth: 2)
+                                .scaleEffect(tap ? 0 : animationStroke)
+                                .opacity(Double(2 - animationStroke))
+                                .animation(
+                                         Animation.easeOut(duration: 3)
+
+                                     )
+
+                        )
+
+
+                        .overlay(
+                            Circle()
+                                .stroke((colorItem), lineWidth: 1)
+                                .scaleEffect(tap ? 0 : explodeStroke)
+                                .opacity(Double(2 - explodeStroke))
+                                .animation(
+                                         Animation.easeOut(duration: 0)
+
+                                     )
+
+                        )
 
                 }
+
+
 
                 .padding()
             }
 
+            .gesture(
+                LongPressGesture().updating($tap) { currentState, gestureState, transaction in
+                    gestureState = currentState
+
+
+                    item.completed.toggle()
+
+                    if item.completed {
+                           // Trigger Custom Haptics
+                        self.press = true
+                            do {
+                                try? engine?.start()
+
+                                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                                let parameter = CHHapticParameterCurve(
+                                    parameterID: .hapticIntensityControl,
+                                    controlPoints: [start, end],
+                                    relativeTime: 0
+                                )
+                                // Play the Haptic that was set up above
+
+                                let event1 = CHHapticEvent(
+                                    eventType: .hapticTransient,
+                                    parameters: [intensity, sharpness],
+                                    relativeTime: 0
+                                )
+
+                                let event2 = CHHapticEvent(
+                                    eventType: .hapticContinuous,
+                                    parameters: [intensity, sharpness],
+                                    relativeTime: 0.125,
+                                    duration: 0.15
+                                )
+
+                                let pattern = try CHHapticPattern(events: [event1, event2],
+                                                                  parameterCurves: [parameter]
+                                )
+
+                                let player = try engine?.makePlayer(with: pattern)
+                                try player?.start(atTime: 0)
+                            } catch {
+
+                                // playing haptics didn't work
+                            }
+
+                        } else {
+                            item.completed = false
+                            self.press = false 
+
+                            let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+                                 impactMed.impactOccurred()
+                        }
+
+                }
+            )
+
+
+            .onTapGesture(count: 1) {
+
+
+               // self.press = true
+                item.completed.toggle()
+
+
+                    if item.completed {
+                           // Trigger Custom Haptics
+                        self.press = true
+                            do {
+                                try? engine?.start()
+
+                                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                                let parameter = CHHapticParameterCurve(
+                                    parameterID: .hapticIntensityControl,
+                                    controlPoints: [start, end],
+                                    relativeTime: 0
+                                )
+                                // Play the Haptic that was set up above
+
+                                let event1 = CHHapticEvent(
+                                    eventType: .hapticTransient,
+                                    parameters: [intensity, sharpness],
+                                    relativeTime: 0
+                                )
+
+                                let event2 = CHHapticEvent(
+                                    eventType: .hapticContinuous,
+                                    parameters: [intensity, sharpness],
+                                    relativeTime: 0.125,
+                                    duration: 0.15
+                                )
+
+                                let pattern = try CHHapticPattern(events: [event1, event2],
+                                                                  parameterCurves: [parameter]
+                                )
+
+                                let player = try engine?.makePlayer(with: pattern)
+                                try player?.start(atTime: 0)
+                            } catch {
+
+                                // playing haptics didn't work
+                            }
+
+                        } else {
+                            item.completed = false
+                            self.press = false 
+
+                            let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+                                 impactMed.impactOccurred()
+                        }
+
+                }
+
+
+
         }
+        
+
 
     }
     
@@ -109,3 +272,4 @@ struct ItemRowView_Previews: PreviewProvider {
         ItemRowView(project: Project.example, item: Item.example)
     }
 }
+
