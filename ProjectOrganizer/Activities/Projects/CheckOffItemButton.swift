288783e6a49951413dc0dcf0ea0d571df2baa75f
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-18 15:16:33 -0400

    prototyping: Worked to layout the button design to be brought into the ItemListView

--- ProjectOrganizer/Activities/Projects/CheckOffItemButton.swift
+++ ProjectOrganizer/Activities/Projects/CheckOffItemButton.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct CheckOffItemButton: View {
+
+    @State private var icon = "checkmark.circle.fill"
+    @State private var colorStroke =  Color.green
+    @State private var press = false
     var body: some View {
-        CheckButton()
+        CheckButton(press: $press, colorStroke: $colorStroke, icon: $icon)
     }
 }
 
@@ -24,22 +29,20 @@ extension Color {
 }
 struct CheckButton: View {
     @GestureState var tap = false
-    @State var press = false
+    @Binding var press: Bool
     @State private var animationStroke: CGFloat = 1
     @State private var explodeStroke: CGFloat = 1
-    @State private var colorStroke = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+    @Binding var colorStroke: Color
+    @Binding var icon: String
+    @State private var engine = try? CHHapticEngine()
+    
+
     var body: some View {
         ZStack {
 
-
-
-
-
-
-
-            Image(systemName: "checkmark.circle.fill")
+            Image(systemName: icon)
                 .font(.system(size: 44, weight: .light))
-                .foregroundColor(Color(colorStroke))
+                .foregroundColor((colorStroke))
                 .opacity(press ? 1 : 0)
                 .scaleEffect(press ? 1 : 0)
 
@@ -47,7 +50,7 @@ struct CheckButton: View {
 
         }
 
-        .frame(width: 55, height: 55)
+        .frame(width: 44, height: 44)
         .background(
             ZStack {
                 LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(press ? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) : #colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
@@ -66,12 +69,12 @@ struct CheckButton: View {
         .overlay(
             Circle()
                 .trim(from: press ? 0.001 : 1, to: 1)
-                .stroke(LinearGradient(gradient: Gradient(colors: [Color(colorStroke), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing), style: StrokeStyle(lineWidth: 5, lineCap: .round))
-                .frame(width: 55, height: 55)
+                .stroke(LinearGradient(gradient: Gradient(colors: [((colorStroke)), ((colorStroke))]), startPoint: .topLeading, endPoint: .bottomTrailing), style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                .frame(width: 44, height: 44)
                 .rotationEffect(Angle(degrees: 90))
                 .rotation3DEffect(Angle(degrees: 180),
                     axis: (x: 1, y: 0, z: 0))
-                .shadow(color: Color(colorStroke).opacity(0.3), radius: 5, x: 3, y: 3)
+                .shadow(color: ((colorStroke)).opacity(0.3), radius: 5, x: 3, y: 3)
                 .animation(.easeInOut)
 
         )
@@ -80,7 +83,7 @@ struct CheckButton: View {
 
         .overlay(
             Circle()
-                .stroke(Color(colorStroke), lineWidth: 5)
+                .stroke((colorStroke), lineWidth: 5)
                 .scaleEffect(tap ? 0 : animationStroke)
                 .opacity(Double(2 - animationStroke))
                 .animation(
@@ -96,7 +99,7 @@ struct CheckButton: View {
 
         .overlay(
             Circle()
-                .stroke(Color(colorStroke), lineWidth: 3)
+                .stroke((colorStroke), lineWidth: 3)
                 .scaleEffect(tap ? 0 : animationStroke)
                 .opacity(Double(2 - animationStroke))
                 .animation(
@@ -109,7 +112,7 @@ struct CheckButton: View {
 
         .overlay(
             Circle()
-                .stroke(Color(colorStroke), lineWidth: 2)
+                .stroke((colorStroke), lineWidth: 2)
                 .scaleEffect(tap ? 0 : animationStroke)
                 .opacity(Double(2 - animationStroke))
                 .animation(
@@ -122,7 +125,7 @@ struct CheckButton: View {
 
         .overlay(
             Circle()
-                .stroke(Color(colorStroke), lineWidth: 1)
+                .stroke((colorStroke), lineWidth: 1)
                 .scaleEffect(tap ? 0 : explodeStroke)
                 .opacity(Double(2 - explodeStroke))
                 .animation(
