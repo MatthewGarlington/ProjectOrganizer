f5a8dc17b4217163f3c8a3bd2ce577b3289dcd2e
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-18 15:03:21 -0400

    feat: Added amount varible to reference the Item Entity in Core Data

--- ProjectOrganizer/Activities/Projects/ItemRowViewModel.swift
+++ ProjectOrganizer/Activities/Projects/ItemRowViewModel.swift
@@ -16,15 +16,18 @@ extension ItemRowView {
         var title: String {
             item.itemTitle
         }
-        
+
+        var amount: String {
+            item.itemAmount
+        }
         var icon: String {
             
             if item.completed {
-                return "checkmark.circle"
+                return "checkmark"
             } else if item.priority == 3 {
-                return  "exclamationmark.triangle"
+                return  "exclamationmark"
             } else {
-                return "circle"
+                return "circlebadge"
             }
         }
         var color: String? {
@@ -41,8 +44,85 @@ extension ItemRowView {
             self.project = project
             self.item = item
         }
-        
-        
-        
+    }
+}
+
+extension CheckButton {
+    class ViewModel: ObservableObject {
+
+        let project: Project
+        let item: Item
+
+        var title: String {
+            item.itemTitle
+        }
+
+        var icon: String {
+
+            if item.completed {
+                return "checkmark.circle"
+            } else if item.priority == 3 {
+                return  "exclamationmark.triangle"
+            } else {
+                return "circle"
+            }
+        }
+        var color: String? {
+
+            if item.completed {
+                return project.projectColor
+            } else if item.priority == 3 {
+                return project.projectColor
+            } else {
+                return project.projectColor
+            }
+        }
+         init(project: Project, item: Item) {
+            self.project = project
+            self.item = item
+        }
+
+
+
+    }
+}
+
+extension CheckOffItemButton {
+    class ViewModel: ObservableObject {
+
+        let project: Project
+        let item: Item
+
+        var title: String {
+            item.itemTitle
+        }
+
+        var icon: String {
+
+            if item.completed {
+                return "checkmark.circle"
+            } else if item.priority == 3 {
+                return  "exclamationmark.triangle"
+            } else {
+                return "circle"
+            }
+        }
+        var color: String? {
+
+            if item.completed {
+                return project.projectColor
+            } else if item.priority == 3 {
+                return project.projectColor
+            } else {
+                return project.projectColor
+            }
+        }
+         init(project: Project, item: Item) {
+            self.project = project
+            self.item = item
+        }
+
+
+
     }
 }
