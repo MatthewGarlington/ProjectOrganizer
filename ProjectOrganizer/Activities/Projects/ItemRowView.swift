2cb579195bc353b97a4b2a000e220dd682c92200
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-19 07:52:44 -0400

    feat: Added strike through of ingredient title upon completion
    This gives the user more context when the item is complete to cross of the title of the item in the List

--- ProjectOrganizer/Activities/Projects/ItemRowView.swift
+++ ProjectOrganizer/Activities/Projects/ItemRowView.swift
@@ -28,10 +28,13 @@ struct ItemRowView: View {
         NavigationLink(destination: EditItemView(item: item)) {
             ZStack(alignment: .center) {
                 Label {
-                    HStack {
+                    HStack(spacing: 5) {
                         
-                        Text(item.itemTitle)
-                        Text("(\(item.itemAmount))")
+                        Text(item.itemTitle).strikethrough(item.completed ? true : false)
+                        if item.itemAmount.isEmpty == false {
+                            Text("(\(item.itemAmount))")
+                                .foregroundColor(.secondary)
+                        }
 
 
                     }
@@ -42,6 +45,8 @@ struct ItemRowView: View {
                         .foregroundColor(colorItem)
                         .font(.system(size: 20))
                         .frame(width: 50, height: 50)
+
+                        
 
                         .background(
                             ZStack {
@@ -121,135 +126,138 @@ struct ItemRowView: View {
                                      )
 
                         )
+                        .gesture(
+                            LongPressGesture().updating($tap) { currentState, gestureState, transaction in
+                                gestureState = currentState
+
+
+                                item.completed.toggle()
+
+                                if item.completed {
+                                       // Trigger Custom Haptics
+                                    self.press = true
+                                        do {
+                                            try? engine?.start()
+
+                                            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                                            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                                            let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                                            let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                                            let parameter = CHHapticParameterCurve(
+                                                parameterID: .hapticIntensityControl,
+                                                controlPoints: [start, end],
+                                                relativeTime: 0
+                                            )
+                                            // Play the Haptic that was set up above
+
+                                            let event1 = CHHapticEvent(
+                                                eventType: .hapticTransient,
+                                                parameters: [intensity, sharpness],
+                                                relativeTime: 0
+                                            )
+
+                                            let event2 = CHHapticEvent(
+                                                eventType: .hapticContinuous,
+                                                parameters: [intensity, sharpness],
+                                                relativeTime: 0.125,
+                                                duration: 0.15
+                                            )
+
+                                            let pattern = try CHHapticPattern(events: [event1, event2],
+                                                                              parameterCurves: [parameter]
+                                            )
+
+                                            let player = try engine?.makePlayer(with: pattern)
+                                            try player?.start(atTime: 0)
+                                        } catch {
+
+                                            // playing haptics didn't work
+                                        }
+
+                                    } else {
+                                        item.completed = false
+                                        self.press = false
+
+                                        let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+                                             impactMed.impactOccurred()
+                                    }
+
+                            }
+                        )
+
+
+                        .onTapGesture(count: 1) {
+
+
+                           // self.press = true
+                            item.completed.toggle()
+
+
+                                if item.completed {
+                                       // Trigger Custom Haptics
+                                    self.press = true
+                                        do {
+                                            try? engine?.start()
+
+                                            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                                            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                                            let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                                            let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                                            let parameter = CHHapticParameterCurve(
+                                                parameterID: .hapticIntensityControl,
+                                                controlPoints: [start, end],
+                                                relativeTime: 0
+                                            )
+                                            // Play the Haptic that was set up above
+
+                                            let event1 = CHHapticEvent(
+                                                eventType: .hapticTransient,
+                                                parameters: [intensity, sharpness],
+                                                relativeTime: 0
+                                            )
+
+                                            let event2 = CHHapticEvent(
+                                                eventType: .hapticContinuous,
+                                                parameters: [intensity, sharpness],
+                                                relativeTime: 0.125,
+                                                duration: 0.15
+                                            )
+
+                                            let pattern = try CHHapticPattern(events: [event1, event2],
+                                                                              parameterCurves: [parameter]
+                                            )
+
+                                            let player = try engine?.makePlayer(with: pattern)
+                                            try player?.start(atTime: 0)
+                                        } catch {
+
+                                            // playing haptics didn't work
+                                        }
+
+                                    } else {
+                                        item.completed = false
+                                        self.press = false
+
+                                        let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+                                             impactMed.impactOccurred()
+                                    }
+
+                            }
 
                 }
+
+                
 
 
 
                 .padding()
             }
 
-            .gesture(
-                LongPressGesture().updating($tap) { currentState, gestureState, transaction in
-                    gestureState = currentState
 
-
-                    item.completed.toggle()
-
-                    if item.completed {
-                           // Trigger Custom Haptics
-                        self.press = true
-                            do {
-                                try? engine?.start()
-
-                                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
-                                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-
-                                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
-                                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
-
-                                let parameter = CHHapticParameterCurve(
-                                    parameterID: .hapticIntensityControl,
-                                    controlPoints: [start, end],
-                                    relativeTime: 0
-                                )
-                                // Play the Haptic that was set up above
-
-                                let event1 = CHHapticEvent(
-                                    eventType: .hapticTransient,
-                                    parameters: [intensity, sharpness],
-                                    relativeTime: 0
-                                )
-
-                                let event2 = CHHapticEvent(
-                                    eventType: .hapticContinuous,
-                                    parameters: [intensity, sharpness],
-                                    relativeTime: 0.125,
-                                    duration: 0.15
-                                )
-
-                                let pattern = try CHHapticPattern(events: [event1, event2],
-                                                                  parameterCurves: [parameter]
-                                )
-
-                                let player = try engine?.makePlayer(with: pattern)
-                                try player?.start(atTime: 0)
-                            } catch {
-
-                                // playing haptics didn't work
-                            }
-
-                        } else {
-                            item.completed = false
-                            self.press = false 
-
-                            let impactMed = UIImpactFeedbackGenerator(style: .heavy)
-                                 impactMed.impactOccurred()
-                        }
-
-                }
-            )
-
-
-            .onTapGesture(count: 1) {
-
-
-               // self.press = true
-                item.completed.toggle()
-
-
-                    if item.completed {
-                           // Trigger Custom Haptics
-                        self.press = true
-                            do {
-                                try? engine?.start()
-
-                                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
-                                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-
-                                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
-                                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
-
-                                let parameter = CHHapticParameterCurve(
-                                    parameterID: .hapticIntensityControl,
-                                    controlPoints: [start, end],
-                                    relativeTime: 0
-                                )
-                                // Play the Haptic that was set up above
-
-                                let event1 = CHHapticEvent(
-                                    eventType: .hapticTransient,
-                                    parameters: [intensity, sharpness],
-                                    relativeTime: 0
-                                )
-
-                                let event2 = CHHapticEvent(
-                                    eventType: .hapticContinuous,
-                                    parameters: [intensity, sharpness],
-                                    relativeTime: 0.125,
-                                    duration: 0.15
-                                )
-
-                                let pattern = try CHHapticPattern(events: [event1, event2],
-                                                                  parameterCurves: [parameter]
-                                )
-
-                                let player = try engine?.makePlayer(with: pattern)
-                                try player?.start(atTime: 0)
-                            } catch {
-
-                                // playing haptics didn't work
-                            }
-
-                        } else {
-                            item.completed = false
-                            self.press = false 
-
-                            let impactMed = UIImpactFeedbackGenerator(style: .heavy)
-                                 impactMed.impactOccurred()
-                        }
-
-                }
 
 
 
