081501d015c4fb3b81272c1a7db14789e4df5e78
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-21 10:47:23 -0400

    prototyping: View deleted

--- ProjectOrganizer/ShoppingList/Home/HomeSettingsView.swift
+++ ProjectOrganizer/ShoppingList/Home/HomeSettingsView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct HomeSettingsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+    @ObservedObject var project: Project
+
+    @EnvironmentObject var dataController: DataController
+    @Environment(\.presentationMode) var presentationMode
+
+
+
+    // Added to make sure scaling down to smaller devices doesn't make the tappable items too small
+    let colorColums = [
+        GridItem(.adaptive(minimum: 44))
+    ]
+    
+    init(project: Project) {
+        self.project = project
+ 
+
+
     }
+    var body: some View {
+        Form {
+            Section(header: Text("Turn Color to Red")) {
+                Button("Project Red" ) {
+
+                 //   project.redbackground.toggle()
+                    dataController.save()
+
+                }
+            }
+        }
+       
+
+    }
+
+
 }
 
 struct HomeSettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeSettingsView()
+        HomeSettingsView(project: Project.example)
     }
 }
