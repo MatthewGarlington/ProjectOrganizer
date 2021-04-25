037c9203c3e8d8930bcdb67906eba7b4adea0570
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-25 08:46:30 -0400

    Access: Add Blue color for launch of foregorund, background, and accent on new launch for accessibility

--- ProjectOrganizer/ShoppingList/Home/HomeView.swift
+++ ProjectOrganizer/ShoppingList/Home/HomeView.swift
@@ -15,6 +15,7 @@ struct HomeView: View {
     @EnvironmentObject var dataController: DataController
     @StateObject var store = UserSettings()
 
+
     
 
 
@@ -23,19 +24,26 @@ struct HomeView: View {
         [GridItem(.fixed(100))]
     }
 
-    init(dataController: DataController) {
+    init(dataController: DataController, tabSelection: Binding<Int>) {
         let viewModel = ViewModel(dataController: dataController)
         _viewModel = StateObject(wrappedValue: viewModel)
+        _tabSelection = tabSelection
+
     }
 
     @State var show = true
     @State var showAwardsModal = false
     @State var waveEffect = false
     @State var rotateEffect = false
-    @State var showSettingsModal = false
     @State var backgroundColor = "Light Blue"
     @State var red = 2
     @State private var tapped = 0
+    @State private var isShow = false
+    @State private var isRewardsShow = false
+    @State private var showDetail = false
+    @State private var showAddScreen = false
+    @State private var noItem = false
+    @Binding var tabSelection: Int
     let delaySeconds = 0.7
     
     var body: some View {
@@ -66,6 +74,8 @@ struct HomeView: View {
                         .offset(x: 175, y: 390)
 
             }
+                .frame(maxWidth: UIScreen.main.bounds.width,
+                                  maxHeight: UIScreen.main.bounds.height)
 
                     .environmentObject(store)
                 ZStack {
@@ -119,70 +129,150 @@ struct HomeView: View {
                             
                             //                  Button("Add Data", action: viewModel.addSampleData)
                             Button(action: {
-                                self.showAwardsModal = true
+                                withAnimation {
+                                self.isRewardsShow.toggle()
+                                    if isRewardsShow {
+                                    self.isShow = false
+                                    }
+                                }
                             }, label: {
 
                                 ZStack {
                                     Circle()
                                         .fill(Color.black.opacity(0.5))
-                                        .frame(width: 75, height: 75)
+                                        .frame(width: 50, height: 50)
                                         .overlay(
                                             Circle()
                                                 .stroke(lineWidth: 4)
                                         )
                                     Image(systemName: "rosette")
-                                        .font(.largeTitle)
+                                        .font(.title)
                                 }
                             })
 
                             Button(action: {
-                                self.showSettingsModal = true
+                                withAnimation {
+                                self.isShow.toggle()
+                                    if isShow {
+                                    self.isRewardsShow = false
+                                    }
+                                }
                             }, label: {
 
                                 ZStack {
                                     Circle()
                                         .fill(Color.black.opacity(0.5))
-                                        .frame(width: 75, height: 75)
+                                        .frame(width: 50, height: 50)
                                         .overlay(
                                             Circle()
                                                 .stroke(lineWidth: 4)
                                         )
                                     Image(systemName: "gear")
-                                        .font(.largeTitle)
+                                        .font(.title)
                                 }
                             })
                         }
                         .padding(.horizontal)
 
                         VStack(alignment: .leading) {
-                            ItemListView(title: "To Get", items: viewModel.moreToExplore, waveEffect: $waveEffect, rotateEffect: $rotateEffect, tapped: $tapped)
+                            ItemListView(title: "To Get", items: viewModel.moreToExplore, waveEffect: $waveEffect, rotateEffect: $rotateEffect, tapped: $tapped, noItem: $noItem)
+
+                            if noItem {
+
+
+
+                                    VStack {
+                                        Button(action: {
+
+                                            self.showAddScreen.toggle()
+                                            self.tabSelection = 2 
+
+                                        },label: {
+                                            ZStack(alignment: .center) {
+                                            RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                                                .fill(Color.black.opacity(0.4))
+                                                .frame(maxWidth: .infinity)
+                                                .frame(height: 150)
+                                                .overlay(
+                                                    RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                                                        .stroke(LinearGradient(gradient: Gradient(colors: [Color.white,
+                                                                                                           Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))]),
+                                                                               startPoint: .leading, endPoint: .trailing),
+                                                                lineWidth: 2)
+
+                                                )
+                                                .shadow(color: Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 20, x: 20, y: 0)
+
+                                           Text("Add Ingredient")
+                                            .padding()
+                                        }
+                                        })
+
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                                    .padding()
+
+                                }
+                                    .offset(y: 100)
+
+                            }
                         }
                         .padding(.horizontal)
                     }
                     .padding()
-                    .sheet(isPresented: $showAwardsModal) {
-                        AwardsView()
-                    }
-                    .sheet(isPresented: $showSettingsModal) {
-                       UserDefaultsView()
+                }
+                if isShow {
+                    ZStack {
+
+                    Spacer()
+                        .background(Color.white.opacity(0.0004))
+                        .frame(maxWidth: UIScreen.main.bounds.width,
+                                          maxHeight: UIScreen.main.bounds.height - 320)
+                    SettingsDetailView(isShow: $isShow)
                         .environmentObject(store)
 
+                    }
+                    .transition(.move(edge: .bottom))
+                    .animation(.easeInOut)
+                }
+
+                if isRewardsShow {
+                    ZStack {
+
+                    Spacer()
+                        .background(Color.white.opacity(0.00004))
+                        .frame(maxWidth: UIScreen.main.bounds.width,
+                                          maxHeight: UIScreen.main.bounds.height - 320)
+                    AwardsModalView(isRewardsShow: $isRewardsShow)
 
                     }
-                    
+                    .transition(.move(edge: .bottom))
+                    .animation(.easeInOut)
                 }
 
             }
             .navigationBarHidden(true)
-
-
         }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView(dataController: .preview)
+        HomeView(dataController: .preview, tabSelection: .constant(1))
+    }
+}
+
+struct BlankView : View {
+
+    var bgColor: Color
+
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(bgColor)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
