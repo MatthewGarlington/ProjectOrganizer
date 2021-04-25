c496b93e3970bdb2fd6afee1e086aad7c6f6de49
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-25 08:43:05 -0400

    prototyping: NO View added

--- ProjectOrganizer/ShoppingList/Home/NoItemShowingAdd.swift
+++ ProjectOrganizer/ShoppingList/Home/NoItemShowingAdd.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct NoItemShowingAdd: View {
+    @Binding var showAddScreen: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .center) {
+            Circle()
+                .fill(Color.black.opacity(0.4))
+                .frame(maxWidth: .infinity)
+                .frame(height: 150)
+                .overlay(
+                   Circle()
+                        .stroke(LinearGradient(gradient: Gradient(colors: [Color.white,
+                                                                           Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))]),
+                                               startPoint: .leading, endPoint: .trailing),
+                                lineWidth: 2)
+                )
+            VStack {
+                Button(action: {
+
+                    self.showAddScreen.toggle()
+
+                },label: {
+                   Text("Add Recipe")
+                    .padding()
+                })
+
+
+            }
+
+            .foregroundColor(.white)
+            .font(.title3)
+            .padding()
+
+        }
     }
 }
 
 struct NoItemShowingAdd_Previews: PreviewProvider {
     static var previews: some View {
-        NoItemShowingAdd()
+        NoItemShowingAdd(showAddScreen: .constant(false))
     }
 }
