bc7c06cd2e73bbaf25745cf14ab8fda999ddfa15
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-25 07:14:41 -0400

    feat: Added View Model for PurchasedView
    The added viewmodel presents only completed items from the shopping list in the PurchasedView. This was done by calling the predicate of completed from the Core Data View Model in form of a true boolean to show in the PurchasedList

--- ProjectOrganizer/ShoppingList/Recipes/PurchasedViewModel.swift
+++ ProjectOrganizer/ShoppingList/Recipes/PurchasedViewModel.swift
@@ -5,4 +5,90 @@
 //  Created by Matthew Garlington on 4/24/21.
 //
 
+
 import Foundation
+import CoreData
+import SwiftUI
+
+extension PurchasedView {
+    class ViewModel: NSObject, ObservableObject, NSFetchedResultsControllerDelegate {
+        private let projectsController: NSFetchedResultsController<Project>
+        private let itemsController:  NSFetchedResultsController<Item>
+
+
+        @Published var projects = [Project]()
+        @Published var items = [Item]()
+
+        var dataController: DataController
+
+
+
+        var moreToExplore: ArraySlice<Item> {
+            items.dropLast(0)
+        }
+
+        init(dataController: DataController) {
+            self.dataController = dataController
+
+            // Construct a Fetch Request to Show all Open Projects
+            let projectRequest: NSFetchRequest<Project> = Project.fetchRequest()
+            projectRequest.predicate = NSPredicate(format: "closed = false")
+            projectRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Project.title, ascending: true)]
+
+            projectsController = NSFetchedResultsController(fetchRequest: projectRequest,
+                                                            managedObjectContext: dataController.container.viewContext,
+                                                            sectionNameKeyPath: nil,
+                                                            cacheName: nil
+            )
+            // Construct a Fetch Request to Show the 10 Priority Items,
+            // incomplete Items from open projects
+
+            let itemRequest: NSFetchRequest<Item> = Item.fetchRequest()
+
+            let completedPredicate = NSPredicate(format: "completed = true")
+            let openPredicate = NSPredicate(format: "project.closed = false")
+            let compoundPredicate = NSCompoundPredicate(type: .and, subpredicates: [completedPredicate, openPredicate])
+            itemRequest.predicate = compoundPredicate
+
+            itemRequest.sortDescriptors = [
+                NSSortDescriptor(keyPath: \Item.priority, ascending: false)
+            ]
+
+            itemRequest.fetchLimit = 10
+
+            itemsController = NSFetchedResultsController(fetchRequest: itemRequest,
+                                                         managedObjectContext: dataController.container.viewContext,
+                                                         sectionNameKeyPath: nil,
+                                                         cacheName: nil
+            )
+
+            super.init()
+
+            projectsController.delegate = self
+            itemsController.delegate = self
+
+            do {
+                try projectsController.performFetch()
+                try itemsController.performFetch()
+                projects = projectsController.fetchedObjects ?? []
+                items = itemsController.fetchedObjects ?? []
+            } catch {
+                print("Failed to Fetch initial data")
+            }
+        }
+
+        func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+            if let newItems = controller.fetchedObjects as? [Item] {
+                items = newItems
+            } else if let newProjects = controller.fetchedObjects as? [Project] {
+                projects = newProjects
+            }
+        }
+
+        func addSampleData() {
+            dataController.deleteAll()
+            try? dataController.createSampleData()
+        }
+    }
+}
+
