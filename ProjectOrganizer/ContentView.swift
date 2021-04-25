6b87b418192f2a01ad7b412556dc723a7d7e7b3f
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-25 08:51:50 -0400

    feat: Added call to added PurchasedView

--- ProjectOrganizer/ContentView.swift
+++ ProjectOrganizer/ContentView.swift
@@ -11,42 +11,43 @@ import SwiftUI
 /// Adds  TabView that allows the user to select between Home, Open Projects, Closed Projects,
 /// and awards that the user has earned
 struct ContentView: View {
-    @SceneStorage("selectedView") var selectedView: String?
+ //   @SceneStorage("selectedView") var selectedView: String?
     @EnvironmentObject var dataController: DataController
     @ObservedObject var userSettings = UserSettings()
+    @State private var tabSelection = 1
     
     init() {
         UITabBar.appearance().barTintColor = UIColor.init(ciColor: .clear)
      }
     
     var body: some View {
-        TabView(selection: $selectedView) {
-            HomeView(dataController: dataController)
-                .tag(HomeView.tag)
+        TabView(selection: $tabSelection) {
+            HomeView(dataController: dataController, tabSelection: $tabSelection)
+                .tag(1)
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }
             ProjectsView(dataController: dataController, showClosedProjects: false)
-                .tag(ProjectsView.openTag)
+                .tag(2)
                 .tabItem {
                     Image(systemName: "list.bullet")
                     Text("Shopping List")
                 }
-            ProjectsView(dataController: dataController, showClosedProjects: true)
-                .tag(ProjectsView.closedTag)
+            PurchasedView(dataController: dataController, showClosedProjects: true)
+                .tag(3)
                 .tabItem {
                     Image(systemName: "checkmark")
                     Text("Purchased")
                 }
             SavedRecipeView(dataController: dataController, showSavedProjects: true)
-                .tag(SavedRecipeView.savedTag)
+                .tag(4)
                 .tabItem {
                     Image(systemName: "star")
                     Text("Saved Recipes")
                 }
             RecipesForTheWeekView(dataController: dataController, mealsThisWeek: true)
-                .tag(RecipesForTheWeekView.weekList)
+                .tag(5)
                 .tabItem {
                     Image(systemName: "calendar.circle")
                     Text("Week")
