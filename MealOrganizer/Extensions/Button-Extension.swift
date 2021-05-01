fb754546b75f87751ba2f5c4b0852ca2612ccc57
Matthew Garlington <SBMatthewG@gmail.com>
2021-05-01 13:49:40 -0400

    refactor: Moved Subviews from HomeView to stand alone file

---
+++ MealOrganizer/Extensions/Button-Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Button-Extension.swift
+//  MealOrganizer
+//
+//  Created by Matthew Garlington on 5/1/21.
+//
+
+import SwiftUI
+
+struct Button_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Button_Extension_Previews: PreviewProvider {
+    static var previews: some View {
+        Button_Extension()
+    }
+}
---
+++ MealOrganizer/ShoppingList/Purchased/PurchasedListView.swift
@@ -0,0 +1,20 @@
+//
+//  PurchasedListView.swift
+//  MealOrganizer
+//
+//  Created by Matthew Garlington on 5/1/21.
+//
+
+import SwiftUI
+
+struct PurchasedListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PurchasedListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PurchasedListView()
+    }
+}
